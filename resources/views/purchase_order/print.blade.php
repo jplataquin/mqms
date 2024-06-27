7d156be13e72e63cb06825823aad21928fb05e22
jplataquin <jp.lataquin@gmail.com>
2024-06-27 15:04:26 +0800

    jspdf

--- resources/views/purchase_order/print.blade.php
+++ resources/views/purchase_order/print.blade.php
@@ -120,25 +120,25 @@
             // console.log(printable.innerHTML);
             // doc.text(printable.innerHTML,10,10);
             // doc.save("two-by-four.pdf");
-            let text = document.getElementById('printable').innerHTML;
-            function printDiv({divId, title}) {
-                let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
+            // let text = document.getElementById('printable').innerHTML;
+            // function printDiv({divId, title}) {
+            //     let mywindow = window.open('', 'PRINT', 'height=650,width=900,top=100,left=150');
 
-                mywindow.document.write(`<html><head><title>${title}</title>`);
-                mywindow.document.write('</head><body >');
-                mywindow.document.write(text);
-                mywindow.document.write('</body></html>');
+            //     mywindow.document.write(`<html><head><title>${title}</title>`);
+            //     mywindow.document.write('</head><body >');
+            //     mywindow.document.write(text);
+            //     mywindow.document.write('</body></html>');
 
-                mywindow.document.close(); // necessary for IE >= 10
-                mywindow.focus(); // necessary for IE >= 10*/
+            //     mywindow.document.close(); // necessary for IE >= 10
+            //     mywindow.focus(); // necessary for IE >= 10*/
 
-                mywindow.print();
-                mywindow.close();
+            //     mywindow.print();
+            //     mywindow.close();
 
-                return true;
-                }
+            //     return true;
+            //     }
 
-                printDiv('adad','asdad');
+            //     printDiv('adad','asdad');
         </script>
     </body>
 </html>
