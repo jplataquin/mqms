9a80b4836d06109c1658c610cdfab56fa60cbbc0
jplataquin <jp.lataquin@gmail.com>
2024-06-28 08:55:53 +0800

    po pdf

--- resources/views/purchase_order/print.blade.php
+++ resources/views/purchase_order/print.blade.php
@@ -3,6 +3,11 @@
          <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     </head>
     <body>
-      
+        <div>
+            <table>
+                <tr>
+</tr>
+            </table>
+        </div>
     </body>
 </html>
