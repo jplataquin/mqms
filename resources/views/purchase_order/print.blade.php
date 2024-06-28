cb16303d5e08afbdeac7ddb85d66e6b6be14b4eb
jplataquin <jp.lataquin@gmail.com>
2024-06-28 09:40:01 +0800

    po pdf

--- resources/views/purchase_order/print.blade.php
+++ resources/views/purchase_order/print.blade.php
@@ -46,34 +46,40 @@
                     </td>
                 </tr>
                 <tr>
-                    <td class="text-left bold" style="width:100px">PO#</td>
-                    <td>{{$purchase_order->id}}</td>
-                    <td class="text-left bold">Supplier</td>
-                    <td>{{$supplier->name}}</td>
-                </tr>
-                <tr>
-                    <td class="text-left bold">Mat. Qty Req #</td>
-                    <td>{{$material_quantity_request->id}}</td>
-                    <td class="text-left bold">Payment Terms</td>
-                    <td>{{$payment_term->text}}</td>
-                </tr>
-                <tr>
-                    <td class="text-left bold">Copy</td>
-                    <td>{{$copy}}</td>
-                    <td class="text-left bold">Contact Person</td>
-                    <td>{{$supplier->primary_contact_person}}</td>
-                </tr>
-                <tr>
-                    <td class="text-left bold">Date</td>
-                    <td>{{$purchase_order->approved_at}}</td>
-                    <td class="text-left bold">Contact No:</td>
-                    <td>{{$supplier->primary_contact_no}}</td>
-                </tr>
-                <tr>
-                    <td class="text-left bold">Project</td>
-                    <td>{{$project->name}}</td>
-                    <td class="text-left bold">Section / Component </td>
-                    <td>{{$section->name}} - {{$component->name}}</td>
+                    <td colspan="4">
+                        <table border="1">
+                            <tr>
+                                <td class="text-left bold" style="width:100px">PO#</td>
+                                <td>{{$purchase_order->id}}</td>
+                                <td class="text-left bold">Supplier</td>
+                                <td>{{$supplier->name}}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-left bold">Mat. Qty Req #</td>
+                                <td>{{$material_quantity_request->id}}</td>
+                                <td class="text-left bold">Payment Terms</td>
+                                <td>{{$payment_term->text}}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-left bold">Copy</td>
+                                <td>{{$copy}}</td>
+                                <td class="text-left bold">Contact Person</td>
+                                <td>{{$supplier->primary_contact_person}}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-left bold">Date</td>
+                                <td>{{$purchase_order->approved_at}}</td>
+                                <td class="text-left bold">Contact No:</td>
+                                <td>{{$supplier->primary_contact_no}}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-left bold">Project</td>
+                                <td>{{$project->name}}</td>
+                                <td class="text-left bold">Section / Component </td>
+                                <td>{{$section->name}} - {{$component->name}}</td>
+                            </tr>
+                        </table>
+                    </td>
                 </tr>
                 <tr>
                     <th class="text-center bold" colspan="4">
