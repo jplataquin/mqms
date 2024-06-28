01e61036ef33e0e92d8d0bb2f57ec9c6da059d8d
jplataquin <jp.lataquin@gmail.com>
2024-06-28 12:03:15 +0800

    po pdf

--- resources/views/purchase_order/print.blade.php
+++ resources/views/purchase_order/print.blade.php
@@ -49,7 +49,7 @@
                                 <td class="text-left bold" style="width:20%">PO#</td>
                                 <td style="width:30%">{{$purchase_order->id}}</td>
                                 <td class="text-left bold"  style="width:20%">Supplier</td>
-                                <td style="width:30%">{{$supplier->name}}</td>
+                                <td style="width:25%">{{$supplier->name}}</td>
                             </tr>
                             <tr>
                                 <td class="text-left bold">Mat. Qty Req #</td>
