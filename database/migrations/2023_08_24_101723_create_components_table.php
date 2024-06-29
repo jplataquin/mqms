933e3604ced457b2c3aba8433e3fb6b89d595d62
jplataquin <jp.lataquin@gmail.com>
2024-06-29 13:52:58 +0800

    rejected and table fix

--- database/migrations/2023_08_24_101723_create_components_table.php
+++ database/migrations/2023_08_24_101723_create_components_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->bigInteger('rejected_by')->nullable();
             
             $table->dateTime('approved_at')->nullable();
-            $table->dateTime('rejected_at')->nullable()
+            $table->dateTime('rejected_at')->nullable();
             $table->softDeletes();
             $table->timestamps();
 
