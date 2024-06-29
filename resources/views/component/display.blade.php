56f9c4a4daf212e47058abb8102f57c5c5d434e3
jplataquin <jp.lataquin@gmail.com>
2024-06-29 14:57:57 +0800

    component fix

--- resources/views/component/display.blade.php
+++ resources/views/component/display.blade.php
@@ -36,7 +36,7 @@
                 <tr>
                     <th>Updated By</th>
                     <td>
-                        {{$component->UpdatedByUser()->name}} at  {{$component->updated_at}}
+                       at  {{$component->updated_at}}
                     </td>
                 </tr>
 
