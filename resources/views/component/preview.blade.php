a71f0b8213f29bb0a3a9950e801c67f7a69f85e0
jplataquin <jp.lataquin@gmail.com>
2024-06-30 16:49:34 +0800

    component print

--- resources/views/component/preview.blade.php
+++ resources/views/component/preview.blade.php
@@ -10,11 +10,14 @@
     .table th{
         padding: 3px;
     }
+
+    .text-center{
+        text-align:center;
+    }
 </style>
 <page>
     <div>
-
-        <div style="margin-bottm: 5px">
+        <div style="margin-bottom: 10px">
             <h3>
                 {{$project->name}} - ( {{$section->name}} )
             </h3>
@@ -45,19 +48,20 @@
         </div>
 
         @foreach($componentItems as $item)
+        <div style="margin-bottom:10px">
         <table border="1" class="table" style="margin-bottom:5px">
             <tr>
                 <th style="width:50%; background-color:#add8e6">{{$item->name}}</th>
-                <th style="width:50%; background-color:#add8e6"> {{$item->quantity}} {{$item->unit}} </th>
+                <th style="width:50%; background-color:#add8e6" class="text-center"> {{$item->quantity}} {{$item->unit}} / P{{number_format($item->budget_price,2) }}</th>
             </tr>
         </table>
         <table border="1" class="table" style="margin-bottom:5px">
            
                 <tr>
-                    <th style="width:25%">Material</th>
-                    <th style="width:25%">Equivalent</th>
-                    <th style="width:25%">Quantity</th>
-                    <th style="width:25%">Total</th>
+                    <th style="width:40%" class="text-center">Material</th>
+                    <th style="width:20%" class="text-center">Equivalent</th>
+                    <th style="width:20%" class="text-center">Quantity</th>
+                    <th style="width:20%" class="text-center">Total</th>
                 </tr>
                 
                 @foreach($item->materialQuantities as $mq)
@@ -67,20 +71,20 @@
                         {{$materialItems[$mq->material_item_id]->specification_unit_packaging }} 
                         {{$materialItems[$mq->material_item_id]->brand }} 
                     </td>
-                    <td>
+                    <td class="text-center">
                         {{$mq->equivalent}} {{$item->unit}}
                     </td>
-                    <td>
+                    <td class="text-center">
                         {{$mq->quantity}}
                     </td>
-                    <td>
+                    <td class="text-center">
                         {{$mq->equivalent * $mq->quantity}} {{$item->unit}}
                     </td>
                 </tr>
                 @endforeach
 
         </table>
-        
+        </div>
         @endforeach
     
 
