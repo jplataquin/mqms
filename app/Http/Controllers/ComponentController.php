cb0c400fd3eec8e79654a496e931dd966c55539b
jplataquin <jp.lataquin@gmail.com>
2024-06-30 14:39:04 +0800

    component print

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -162,7 +162,7 @@ class ComponentController extends Controller
         ])->render();
 
 
-        $html2pdf = new Html2Pdf('P','A4','en', false, 'UTF-8', [5, 0, 5, 0]);
+        $html2pdf = new Html2Pdf('P','A4','en', false, 'UTF-8', [5, 0, 10, 0]);
            
 
         try {
