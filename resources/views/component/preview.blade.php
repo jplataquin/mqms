6110955b651d6636a1c41f97654bf84fc202ac7f
jplataquin <jp.lataquin@gmail.com>
2024-06-30 17:06:37 +0800

    component print

--- resources/views/component/preview.blade.php
+++ resources/views/component/preview.blade.php
@@ -94,14 +94,16 @@
 
         <nobreak>
             <div style="margin-bottom:10px">
-                <table border="1" class="table" style="margin-bottom:5px">
+                <table border="1" class="table" style="margin-botom:3px">
                     <tr>
-                        <th style="width:50%; background-color:#add8e6">{{$item->name}}</th>
-                        <th style="width:50%; background-color:#add8e6" class="text-center"> {{$item->quantity}} {{$item->unit}} / P{{number_format($item->budget_price,2) }}</th>
+                        <th style="width:50%;background-color:#add8e6" colspan="2">{{$item->name}}</th>
+                        <th style="width:25%;background-color:#add8e6" class="text-center">{{$item->quantity}} {{$item->unit}}</th>
+                        <th style="width:25%;background-color:#add8e6" class="text-center">P {{number_format($item->budget_price,2) }}</th>
                     </tr>
                 </table>
                 <table border="1" class="table" style="margin-bottom:5px">
-                
+                        
+
                         <tr>
                             <th style="width:40%" class="text-center">Material</th>
                             <th style="width:20%" class="text-center">Equivalent</th>
@@ -131,6 +133,49 @@
                 </table>
             </div>
         </nobreak>
+
+        <nobreak>
+            <div style="margin-bottom:10px">
+                <table border="1" class="table" style="margin-botom:3px">
+                    <tr>
+                        <th style="width:50%;background-color:#add8e6" colspan="2">{{$item->name}}</th>
+                        <th style="width:25%;background-color:#add8e6" class="text-center">{{$item->quantity}} {{$item->unit}}</th>
+                        <th style="width:25%;background-color:#add8e6" class="text-center">P {{number_format($item->budget_price,2) }}</th>
+                    </tr>
+                </table>
+                <table border="1" class="table" style="margin-bottom:5px">
+                        
+
+                        <tr>
+                            <th style="width:40%" class="text-center">Material</th>
+                            <th style="width:20%" class="text-center">Equivalent</th>
+                            <th style="width:20%" class="text-center">Quantity</th>
+                            <th style="width:20%" class="text-center">Total</th>
+                        </tr>
+                        
+                        @foreach($item->materialQuantities as $mq)
+                        <tr>
+                            <td>
+                                {{$materialItems[$mq->material_item_id]->name }} 
+                                {{$materialItems[$mq->material_item_id]->specification_unit_packaging }} 
+                                {{$materialItems[$mq->material_item_id]->brand }} 
+                            </td>
+                            <td class="text-center">
+                                {{$mq->equivalent}} {{$item->unit}}
+                            </td>
+                            <td class="text-center">
+                                {{$mq->quantity}}
+                            </td>
+                            <td class="text-center">
+                                {{$mq->equivalent * $mq->quantity}} {{$item->unit}}
+                            </td>
+                        </tr>
+                        @endforeach
+
+                </table>
+            </div>
+        </nobreak>
+
         @endforeach
     
 
