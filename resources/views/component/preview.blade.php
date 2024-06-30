47b4d925413a6a1ebf25e888c8cf238276bc6f97
jplataquin <jp.lataquin@gmail.com>
2024-06-30 16:57:26 +0800

    component print

--- resources/views/component/preview.blade.php
+++ resources/views/component/preview.blade.php
@@ -48,45 +48,7 @@
         </div>
 
         @foreach($componentItems as $item)
-        <nobreak>
-        <div style="margin-bottom:10px">
-        <table border="1" class="table" style="margin-bottom:5px">
-            <tr>
-                <th style="width:50%; background-color:#add8e6">{{$item->name}}</th>
-                <th style="width:50%; background-color:#add8e6" class="text-center"> {{$item->quantity}} {{$item->unit}} / P{{number_format($item->budget_price,2) }}</th>
-            </tr>
-        </table>
-        <table border="1" class="table" style="margin-bottom:5px">
-           
-                <tr>
-                    <th style="width:40%" class="text-center">Material</th>
-                    <th style="width:20%" class="text-center">Equivalent</th>
-                    <th style="width:20%" class="text-center">Quantity</th>
-                    <th style="width:20%" class="text-center">Total</th>
-                </tr>
-                
-                @foreach($item->materialQuantities as $mq)
-                <tr>
-                    <td>
-                        {{$materialItems[$mq->material_item_id]->name }} 
-                        {{$materialItems[$mq->material_item_id]->specification_unit_packaging }} 
-                        {{$materialItems[$mq->material_item_id]->brand }} 
-                    </td>
-                    <td class="text-center">
-                        {{$mq->equivalent}} {{$item->unit}}
-                    </td>
-                    <td class="text-center">
-                        {{$mq->quantity}}
-                    </td>
-                    <td class="text-center">
-                        {{$mq->equivalent * $mq->quantity}} {{$item->unit}}
-                    </td>
-                </tr>
-                @endforeach
-
-        </table>
-        </div>
-        </nobreak>
+        
 
         <nobreak>
             <div style="margin-bottom:10px">
