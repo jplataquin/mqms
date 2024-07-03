6c5f66953c8d976e163c0b457679d4873373ab9b
jplataquin <jp.lataquin@gmail.com>
2024-07-03 19:11:14 +0800

    reports

--- app/Http/Controllers/Reports/ReportAController.php
+++ app/Http/Controllers/Reports/ReportAController.php
@@ -82,7 +82,6 @@ class ReportAController extends Controller
                 
         }
 
-        print_r($total_requested);exit;
 
         return view('reports/report_a/generate',[
             'project'           => $project,
