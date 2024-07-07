5b0c711ca51cbcaf517a2102a5e772056d874a3b
jplataquin <jp.lataquin@gmail.com>
2024-07-08 00:01:17 +0800

    fix

--- resources/ui_components/ComponentItem.js
+++ resources/ui_components/ComponentItem.js
@@ -513,7 +513,7 @@ class ComponentItem extends Component{
            
             this.setState('budget_price',parseFloat(reply.data.budget_price));
             this.setState('quantity',parseFloat(reply.data.quantity));
-            this.setState('unit',reply.data.unit);
+            this.setState('unit',this._model.unitOptions[reply.data.component_unit_id].text);
             this.setState('name',reply.data.name);
             this.setState('function_type_id',reply.data.function_type_id);
             this.setState('variable',reply.data.function_variable);
@@ -521,7 +521,7 @@ class ComponentItem extends Component{
             this.el.name.value          = reply.data.name;
             this.el.budget_price.value  = reply.data.budget_price;
             this.el.quantity.value      = reply.data.quantity;
-            this.el.unit.value          = reply.data.component_unit_id;
+            this.el.unit.value          = this._model.unitOptions[reply.data.component_unit_id].text;
             this.el.function_type.value = reply.data.function_type_id;
             this.el.variable.value      = reply.data.function_variable;
 
