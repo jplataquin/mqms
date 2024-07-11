f35c03dd28541e4f7936772ec04a00d6db481c36
jplataquin <jp.lataquin@gmail.com>
2024-07-12 01:37:58 +0800

    fix

--- resources/views/section/print.blade.php
+++ resources/views/section/print.blade.php
@@ -10,7 +10,26 @@
                 
                 <tr>
                     <td>{{$contract_item->item_code}}</td>
-                    <td colspan="8">{{$contract_item->description}}</td>
+                    <td>{{$contract_item->description}}</td>
+                    <td>
+                        {{$contract_item->contract_quantity}}
+                    </td>
+                    <td>
+                        {{$unit_options[$contract_item->unit_id]->text}}
+                    </td>
+                    <td>
+                        PHP {{$contract_item->contract_unit_price}}
+                    </td>
+
+                    <td>
+                        {{$contract_item->ref_1_quantity}}
+                    </td>
+                    <td>
+                        {{$unit_options[$contract_item->unit_id]->text}}
+                    </td>
+                    <td>
+                        PHP {{$contract_item->ref_1_unit_price}}
+                    </td>
                 </tr>
 
                 @php
@@ -30,27 +49,12 @@
                         <td>
                             {{$component->name}}
                         </td>
-                        
-                        
-                        <td>
-                            {{$contract_item->contract_quantity}}
-                        </td>
-                        <td>
-                            {{$unit_options[$contract_item->unit_id]->text}}
-                        </td>
-                        <td>
-                            PHP {{$contract_item->contract_unit_price}}
-                        </td>
-
-                        <td>
-                            {{$contract_item->ref_1_quantity}}
-                        </td>
-                        <td>
-                            {{$unit_options[$contract_item->unit_id]->text}}
-                        </td>
-                        <td>
-                            PHP {{$contract_item->ref_1_unit_price}}
-                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                     </tr>    
                 @endforeach
                 
