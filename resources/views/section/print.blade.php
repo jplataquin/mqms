9f68277133351a61a86e00275f2ef3cc6a32dad8
jplataquin <jp.lataquin@gmail.com>
2024-07-12 01:47:17 +0800

    fix

--- resources/views/section/print.blade.php
+++ resources/views/section/print.blade.php
@@ -8,15 +8,13 @@
                 <th rowspan="2">Item Code</th>
                 <th rowspan="2">Description</th>
                 <th colspan="4">Contract</th>
+            </tr>
             <tr>
-            <tr>
-                <th></th>
-                <th></th>
                 <th>QTY</th>
                 <th>UNIT</th>
                 <th>UNIT PRICE</th>
                 <th>AMOUNT</th>
-            <tr>
+            </tr>
                 
             @foreach($contract_items as $contract_item)
 
