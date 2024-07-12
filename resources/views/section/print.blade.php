d9ad3c60983985c0210f73b0f0bfd0f3613a809a
jplataquin <jp.lataquin@gmail.com>
2024-07-12 09:27:53 +0800

    fix

--- resources/views/section/print.blade.php
+++ resources/views/section/print.blade.php
@@ -173,7 +173,7 @@
                                 @endif
                                 
                                 @if($component_item->function_type_id == 2)
-                                    {{ round( ($component_item->quantity / $component_item->function_variable) / $component->use_count,2 ) }} 
+                                    {{ round( (1 / $component_item->function_variable) / $component->use_count,2 ) }} 
                                     {{$unit_options[$component_item->unit_id]->text}}
                                     /
                                     {{$unit_options[$component->unit_id]->text}}     
