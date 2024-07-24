ff106630849ac45755e9cfff37fcb14f365cc1e3
jplataquin <jp.lataquin@gmail.com>
2024-07-24 17:57:46 +0800

    sum_flag_component

--- resources/views/section/print.blade.php
+++ resources/views/section/print.blade.php
@@ -210,7 +210,7 @@
                         <th class="@if($component_item_quantity_total_per_component[$component->id] > $component->quantity) font-color-danger @endif">
                             {{ $component_item_quantity_total_per_component[$component->id] }}
                         </th>
-                        <th>
+                        <th class="@if($component_item_quantity_total_per_component[$component->id] > $component->quantity) font-color-danger @endif">
                             {{$unit_options[$component->unit_id]->text}}
                         </th>
                         
