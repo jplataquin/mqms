8b91d760735fa704ac0d4f0e0538bd4263f509f7
jplataquin <jp.lataquin@gmail.com>
2024-07-25 23:33:32 +0800

    fix

--- resources/views/component/display.blade.php
+++ resources/views/component/display.blade.php
@@ -186,6 +186,8 @@
     </div>
     <div class="row mt-3 border border-warning p-3">
         <h5>POW/DUPA</h5>
+    </div>
+    <div class="row mt-3">
         <div class="col-lg-4">
             <div class="form-group">
                 <label>Quantity</label>
