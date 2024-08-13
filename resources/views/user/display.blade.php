92f7ad0f23d806eaaf1f3fa9c284fd70e22617ad
jplataquin <jp.lataquin@gmail.com>
2024-08-13 17:55:02 +0800

    fix

--- resources/views/user/display.blade.php
+++ resources/views/user/display.blade.php
@@ -123,6 +123,11 @@
 
     <div id="list" class="row mt-3"></div>
 
+    <div class="row mt-3">
+        <div class="col-lg-12">
+            <button id="showMoreBtn" class="btn w-100 btn-primary">Show More</button>
+        </div>
+    </div>
 </div>
 
 <script type="module">
@@ -137,6 +142,7 @@
     const role                 = $q('#role').first();
     const addRoleBtn           = $q('#addRoleBtn').first();
     const list                 = $q('#list').first();
+    const showMoreBtn          = $q('#showMoreBtn').first();
 
 
     let editable_flag = false;
