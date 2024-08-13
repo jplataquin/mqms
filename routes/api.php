15b5940eb0afe689fb5389ba48f029cfe33f4118
jplataquin <jp.lataquin@gmail.com>
2024-08-13 17:52:20 +0800

    fix

--- routes/api.php
+++ routes/api.php
@@ -38,10 +38,11 @@ Route::middleware(['auth:sanctum'])->group(function () {
     
 
     Route::get('/user/list', [App\Http\Controllers\UserController::class, '_list']);
-    Route::get('/user/roles/{$id}', [App\Http\Controllers\UserController::class, '_roles']);
     Route::post('/user/create', [App\Http\Controllers\UserController::class, '_create']);
     Route::post('/user/update', [App\Http\Controllers\UserController::class, '_update']);
     Route::post('/user/reset_password', [App\Http\Controllers\UserController::class, '_reset_password']);
+   
+    Route::get('/user/roles/{$id}', [App\Http\Controllers\UserController::class, '_roles']);
     Route::post('/user/role/add', [App\Http\Controllers\UserController::class, '_add_role']);
     Route::post('/user/role/remove', [App\Http\Controllers\UserController::class, '_remove_role']);
 
