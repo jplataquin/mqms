c18cd6a6ff9e32537a019e5b29b042e9d3cc1f00
jplataquin <jp.lataquin@gmail.com>
2024-08-14 11:34:11 +0800

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,14 +24,16 @@
     <div class="wrapper d-flex h-100" id="app">
         
         <div id="side-bar-container">
-            <div class="text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-            </svg>
-            </div>
-
+            
             <nav id="sidebar" hx-boost="true" class="h-100">
+
+                <div class="text-center mb-10">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                    </svg>
+                </div>
+
                 <a class="d-none" hx-select="#content" hx-target="#main" href="/roles" id="#__nav_helper"></a>
                 <ul class="list-unstyled">
                     <li class="menu-item">
