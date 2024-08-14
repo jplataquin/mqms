160118071a326b2a1cf8faf6b51d160644b27ce5
jplataquin <jp.lataquin@gmail.com>
2024-08-14 11:44:13 +0800

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,11 +27,14 @@
             
             <nav id="sidebar" hx-boost="true" class="h-100">
 
-                <div class="text-center mb-10 mt-10">
+                <div class="text-center mb-3 mt-3">
                     <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="#ffffff" class="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
                     </svg>
+                    <div class="text-center">
+                        {{Auth::user()->name}}
+                    </div>
                 </div>
 
                 <a class="d-none" hx-select="#content" hx-target="#main" href="/roles" id="#__nav_helper"></a>
