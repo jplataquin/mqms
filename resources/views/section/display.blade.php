acde563151b33188cf6c1367fb8378a8b28217a2
jplataquin <jp.lataquin@gmail.com>
2024-08-14 11:22:20 +0800

    fix

--- resources/views/section/display.blade.php
+++ resources/views/section/display.blade.php
@@ -23,6 +23,7 @@
             </li>
         </ul>
     </div>
+    
     <hr>
 
 
@@ -41,34 +42,37 @@
 
     </div>
 
-    
-    <div class="form-header text-center mb-3">
-        Section
-    </div>
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label>Name</label>
-                <input type="text" id="sectionName" value="{{$section->name}}" disabled="true" class="form-control"/>        
+    <div class="form-container">
+        <div class="form-header">
+            Section
+        </div>
+        <div class="form-body">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" id="sectionName" value="{{$section->name}}" disabled="true" class="form-control"/>        
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <label>ID</label>
+                    <input type="text" value="{{str_pad($section->id,6,0,STR_PAD_LEFT)}}" disabled="true" class="form-control"/>
+                </div>
             </div>
-        </div>
-        <div class="col-lg-6">
-            <label>ID</label>
-            <input type="text" value="{{str_pad($section->id,6,0,STR_PAD_LEFT)}}" disabled="true" class="form-control"/>
-        </div>
-    </div>
 
-    <div class="row mt-5">
-        <div class="col-lg-6">
-            <button class="btn btn-danger" id="deleteBtn">Delete</button>
-          
-        </div>
-        <div class="col-lg-6 text-end">
+            <div class="row mt-5">
+                <div class="col-lg-6">
+                    <button class="btn btn-danger" id="deleteBtn">Delete</button>
+                
+                </div>
+                <div class="col-lg-6 text-end">
 
-            <button class="btn btn-warning" id="printBtn">Print</button>
-            <button class="btn btn-secondary" id="cancelBtn">Cancel</button>
-            <button class="btn btn-primary" id="editBtn">Edit</button>
-            <button class="btn btn-warning d-none" id="updateBtn">Update</button>
+                    <button class="btn btn-warning" id="printBtn">Print</button>
+                    <button class="btn btn-secondary" id="cancelBtn">Cancel</button>
+                    <button class="btn btn-primary" id="editBtn">Edit</button>
+                    <button class="btn btn-warning d-none" id="updateBtn">Update</button>
+                </div>
+            </div>
         </div>
     </div>
 
