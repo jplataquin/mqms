46402008f85260f3c3d7fcb64bd8f865197c7d22
jplataquin <jp.lataquin@gmail.com>
2024-08-14 16:12:45 +0800

    fix

--- app/Http/Middleware/CheckForResetPassword.php
+++ app/Http/Middleware/CheckForResetPassword.php
@@ -2,34 +2,27 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
+class CheckForResetPassword
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @param  string|null  ...$guards
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next, ...$guards)
+    public function handle(Request $request, Closure $next)
     {
-        $guards = empty($guards) ? [null] : $guards;
-      
-        foreach ($guards as $guard) {
-            
-            
-            if (Auth::guard($guard)->check()) {
-
-                return redirect(RouteServiceProvider::HOME);
+        if(Auth::check()){
+            if(Auth::user()->reset_password){
+                redirect('/projects');
             }
         }
-
+        
         return $next($request);
     }
 }
