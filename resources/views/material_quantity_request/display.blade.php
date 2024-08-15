e1798e54dae16c6444a99df33bd4ebac800d2937
jplataquin <jp.lataquin@gmail.com>
2024-08-15 23:53:34 +0800

    fix

--- resources/views/material_quantity_request/display.blade.php
+++ resources/views/material_quantity_request/display.blade.php
@@ -91,7 +91,7 @@
     
     const request_items             = @json($request_items);
     const unit_options              = @json($unit_options);
-    const component_item_options    = @json($componentItem_options);
+    const component_item_options    = @json($component_item_options);
     const material_options          = @json($material_options);
     
     request_items.map(request_item => {
