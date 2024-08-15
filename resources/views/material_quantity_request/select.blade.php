1008cb7123bf8f35428a94d7620fe396af831f87
jplataquin <jp.lataquin@gmail.com>
2024-08-15 20:53:47 +0800

    fix

--- resources/views/material_quantity_request/select.blade.php
+++ resources/views/material_quantity_request/select.blade.php
@@ -2,211 +2,216 @@
 
 @section('content')
 <div id="content">
-<div class="container">
-<div class="breadcrumbs" hx-boost="true" hx-select="#content" hx-target="#main">
-        <ul>
-            <li>
-                <a href="/material_quantity_requests">
-                    <span>
-                       Material Quantity Request
-                    </span>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="active">
-                    <span>
-                        Select
-                    </span>
-                    <i class="ms-2 bi bi-ui-checks-grid"></i>		
-                </a>
-            </li>
-        </ul>
+    <div class="container">
+        <div class="breadcrumbs" hx-boost="true" hx-select="#content" hx-target="#main">
+            <ul>
+                <li>
+                    <a href="/material_quantity_requests">
+                        <span>
+                        Material Quantity Request
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="active">
+                        <span>
+                            Select
+                        </span>
+                        <i class="ms-2 bi bi-ui-checks-grid"></i>		
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <hr>
+        
+        <div class="folder-form-container">
+            <div class="folder-form-tab">
+                Material Quantity Select
+            </div>
+            <div class="folder-form-body">
+                <div class="row">
+                    <div class="col-3">
+                        <div class="form-group">
+                            <label>Project</label>
+                            <select id="projectSelect" class="form-control">
+                                <option value=""> - </option>
+                                @foreach($projects as $project)
+                                    <option value="{{$project->id}}">{{$project->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="form-group">
+                            <label>Section</label>
+                            <select id="sectionSelect" class="form-control"></select>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="form-group">
+                            <label>Contract Item</label>
+                            <select id="contractItemSelect" class="form-control"></select>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="form-group">
+                            <label>Component</label>
+                            <select id="componentSelect" class="form-control"></select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mt-3">
+                    <div class="col-8"></div>
+                    <div class="col-2">
+                        <button class="btn w-100 btn-secondary">Cancel</button>
+                    </div>
+                    <div class="col-2">
+                        <button id="createBtn" class="btn w-100 btn-warning">Create</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <hr>
-    
-    <div class="row">
-        <div class="col-3">
-            <div class="form-group">
-                <label>Project</label>
-                <select id="projectSelect" class="form-control">
-                    <option value=""> - </option>
-                    @foreach($projects as $project)
-                        <option value="{{$project->id}}">{{$project->name}}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-        <div class="col-3">
-            <div class="form-group">
-                <label>Section</label>
-                <select id="sectionSelect" class="form-control"></select>
-            </div>
-        </div>
-        <div class="col-3">
-            <div class="form-group">
-                <label>Contract Item</label>
-                <select id="contractItemSelect" class="form-control"></select>
-            </div>
-        </div>
-        <div class="col-3">
-            <div class="form-group">
-                <label>Component</label>
-                <select id="componentSelect" class="form-control"></select>
-            </div>
-        </div>
-    </div>
+    <script type="module">
+        import {$q,Template} from '/adarna.js';
+        
+        const projectSelect         = $q('#projectSelect').first();
+        const sectionSelect         = $q('#sectionSelect').first();
+        const contractItemSelect    = $q('#contractItemSelect').first();
+        const componentSelect       = $q('#componentSelect').first();
+        const createBtn             = $q('#createBtn').first();
+        
+        const t = new Template();
 
-    <div class="row mt-3">
-        <div class="col-8"></div>
-        <div class="col-2">
-            <button class="btn w-100 btn-secondary">Cancel</button>
-        </div>
-        <div class="col-2">
-            <button id="createBtn" class="btn w-100 btn-warning">Create</button>
-        </div>
-    </div>
+        projectSelect.onchange = (e)=>{
 
-</div>
+            e.preventDefault();
 
-<script type="module">
-    import {$q,Template} from '/adarna.js';
-    
-    const projectSelect         = $q('#projectSelect').first();
-    const sectionSelect         = $q('#sectionSelect').first();
-    const contractItemSelect    = $q('#contractItemSelect').first();
-    const componentSelect       = $q('#componentSelect').first();
-    const createBtn             = $q('#createBtn').first();
-    
-    const t = new Template();
+            sectionSelect.innerHTML = '';
+            componentSelect.innerHTML = '';
 
-    projectSelect.onchange = (e)=>{
+            window.util.blockUI();
 
-        e.preventDefault();
+            window.util.$get('/api/section/list',{
+                project_id: projectSelect.value,
+                orderBy:'name',
+                status:'ACTV',
+                order:'ASC'
+            }).then(reply=>{
 
-        sectionSelect.innerHTML = '';
-        componentSelect.innerHTML = '';
+                window.util.unblockUI();
+                    
 
-        window.util.blockUI();
+                if(reply.status <= 0){
 
-        window.util.$get('/api/section/list',{
-            project_id: projectSelect.value,
-            orderBy:'name',
-            status:'ACTV',
-            order:'ASC'
-        }).then(reply=>{
-
-            window.util.unblockUI();
-                
-
-            if(reply.status <= 0){
-
-                window.util.showMsg(reply);
-                return false;
-            }
-
-            sectionSelect.append(
-                t.option({value:''},' - ')
-            );
-
-            reply.data.forEach((item)=>{
+                    window.util.showMsg(reply);
+                    return false;
+                }
 
                 sectionSelect.append(
-                    t.option({value:item.id},item.name)
+                    t.option({value:''},' - ')
                 );
 
+                reply.data.forEach((item)=>{
+
+                    sectionSelect.append(
+                        t.option({value:item.id},item.name)
+                    );
+
+                });
+
             });
+        }
 
-        });
-    }
+        sectionSelect.onchange = (e)=>{
 
-    sectionSelect.onchange = (e)=>{
+            e.preventDefault();
 
-        e.preventDefault();
+            contractItemSelect.innerHTML = '';
 
-        contractItemSelect.innerHTML = '';
+            window.util.blockUI();
 
-        window.util.blockUI();
+            window.util.$get('/api/contract_item/list',{
+                section_id: sectionSelect.value,
+                orderBy:'name',
+                status:'APRV',
+                order:'ASC'
+            }).then(reply=>{
 
-        window.util.$get('/api/contract_item/list',{
-            section_id: sectionSelect.value,
-            orderBy:'name',
-            status:'APRV',
-            order:'ASC'
-        }).then(reply=>{
+                window.util.unblockUI();
 
-            window.util.unblockUI();
+                if(reply.status <= 0){
 
-            if(reply.status <= 0){
-
-                window.util.showMsg(reply);
-                return false;
-            }
-
-            contractItemSelect.append(
-                t.option({value:''},' - ')
-            );
-
-            reply.data.forEach((item)=>{
+                    window.util.showMsg(reply);
+                    return false;
+                }
 
                 contractItemSelect.append(
-                    t.option({value:item.id},'[ '+item.item_code+' ] '+item.description)
+                    t.option({value:''},' - ')
                 );
 
+                reply.data.forEach((item)=>{
+
+                    contractItemSelect.append(
+                        t.option({value:item.id},'[ '+item.item_code+' ] '+item.description)
+                    );
+
+                });
+
             });
-
-        });
-    }
+        }
 
 
-    contractItemSelect.onchange = (e)=>{
+        contractItemSelect.onchange = (e)=>{
 
-        e.preventDefault();
+            e.preventDefault();
 
-        componentSelect.innerHTML = '';
+            componentSelect.innerHTML = '';
 
-        window.util.blockUI();
+            window.util.blockUI();
 
-        window.util.$get('/api/component/list',{
-            contract_item_id: contractItemSelect.value,
-            orderBy:'name',
-            status:'APRV',
-            order:'ASC'
-        }).then(reply=>{
+            window.util.$get('/api/component/list',{
+                contract_item_id: contractItemSelect.value,
+                orderBy:'name',
+                status:'APRV',
+                order:'ASC'
+            }).then(reply=>{
 
-            window.util.unblockUI();
+                window.util.unblockUI();
 
-            if(reply.status <= 0){
+                if(reply.status <= 0){
 
-                window.util.showMsg(reply);
-                return false;
-            }
-
-            componentSelect.append(
-                t.option({value:''},' - ')
-            );
-
-            reply.data.forEach((item)=>{
+                    window.util.showMsg(reply);
+                    return false;
+                }
 
                 componentSelect.append(
-                    t.option({value:item.id},item.name)
+                    t.option({value:''},' - ')
                 );
 
+                reply.data.forEach((item)=>{
+
+                    componentSelect.append(
+                        t.option({value:item.id},item.name)
+                    );
+
+                });
+
             });
+            }
 
-        });
+
+        createBtn.onclick = (e)=>{
+            e.preventDefault();
+
+            if(projectSelect.value == '' || sectionSelect.value == '' || componentSelect.value == ''){
+                window.util.alert('Error','All fields are required to create a request');
+                return false;
+            }
+            
+            widnow.util.navTo('/material_quantity_request/create/'+projectSelect.value+'/'+sectionSelect.value+'/'+componentSelect.value);
         }
-
-
-    createBtn.onclick = (e)=>{
-        e.preventDefault();
-
-        if(projectSelect.value == '' || sectionSelect.value == '' || componentSelect.value == ''){
-            window.util.alert('Error','All fields are required to create a request');
-            return false;
-        }
-        
-        widnow.util.navTo('/material_quantity_request/create/'+projectSelect.value+'/'+sectionSelect.value+'/'+componentSelect.value);
-    }
-</script>
+    </script>
 </div>
 @endsection
