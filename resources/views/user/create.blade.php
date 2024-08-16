40df37450ba0c1aedb211b1c5e9636f1204c7d51
jplataquin <jp.lataquin@gmail.com>
2024-08-16 20:17:36 +0800

    fix

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <div id="content">
 <div class="container">
-<div class="breadcrumbs">
+<div class="breadcrumbs" hx-boost="true" hx-select="#content" hx-target="#main">
         <ul>
             <li>
-                <a href="#">
+                <a href="/users">
                     <span>
                        Users
                     </span>
@@ -16,36 +16,44 @@
                 <a href="#" class="active">
                     <span>
                         Create
-                    </span>		
+                    </span>
+                    <i class="ms-2 bi bi-file-earmark-plus"></i>		
                 </a>
             </li>
         </ul>
     </div>
-<hr>
+    <hr>
 
-    <div class="row mb-3">
-
-        <div class="col-lg-12">
-            <div class="form-group">
-                <label>Name</label>
-                <input type="text" id="name" class="form-control"/>
-            </div>
+    <div class="form-container">
+        <div class="form-header">
+            Create User
         </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="form-group">
-                <label>Email</label>
-                <input type="text" id="email" class="form-control"/>
+        <div class="form-body">
+            <div class="row mb-3">
+
+                <div class="col-lg-12">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" id="name" class="form-control"/>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" id="email" class="form-control"/>
+                    </div>
+                </div>
 
-    </div>
+            </div>
 
-    <div class="row mt-5">
-        <div class="col-12 text-end">
-            <button class="btn btn-secondary" id="cancelBtn">Cancel</button>
-            <button class="btn btn-primary" id="createBtn">Create</button>
+            <div class="row mt-5">
+                <div class="col-12 text-end">
+                    <button class="btn btn-secondary" id="cancelBtn">Cancel</button>
+                    <button class="btn btn-primary" id="createBtn">Create</button>
+                </div>
+            </div>
         </div>
     </div>
 
