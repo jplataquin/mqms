3de6ee1a23b81ac3f4b462ce4cdc83a60c9489c1
jplataquin <jp.lataquin@gmail.com>
2024-08-19 04:35:35 +0800

    fix

--- resources/views/purchase_order/select.blade.php
+++ resources/views/purchase_order/select.blade.php
@@ -128,8 +128,8 @@
         
         data.map(item=>{
 
-            console.log(item);
-            
+            console.log(item.id, item.component);
+
             let row = t.tr({class:'selectable-div'},()=>{
                 t.td(String(item.id).padStart(6,0));
             
