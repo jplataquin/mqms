2419522485ecd162287fd48768647c0bac4445ed
jplataquin <jp.lataquin@gmail.com>
2024-08-19 17:00:17 +0800

    fix

--- resources/ui_components/RequestMaterialItem.js
+++ resources/ui_components/RequestMaterialItem.js
@@ -256,6 +256,7 @@ class RequestMaterialItem extends Component{
             this.getApprovedQuantity(this._state.componentItemId, this._state.materialItemId,false);
         }
 
+        console.log(this._model.materialItemId);
         this.el.componentItemSelect.value       = this._model.componentItemId;
         this.el.materialSelect.value            = this._model.materialItemId;
         this.el.requestedQuantity.value         = this._model.requestedQuantity;
