27779c5573b9948d477ee47823028e528fbbd4d7
jplataquin <jp.lataquin@gmail.com>
2024-08-19 10:53:06 +0800

    fix

--- app/Http/Controllers/MaterialQuantityRequestController.php
+++ app/Http/Controllers/MaterialQuantityRequestController.php
@@ -479,7 +479,7 @@ class MaterialQuantityRequestController extends Controller
             $request_item_arr[$materialQuantityRequest->id.'-'.$rq->component_item_id.'-'.$rq->material_item_id] = $rq;
         }
         
-        print_r($requested_item_arr);exit;
+        print_r($reques_item_arr);exit;
         $material_item_result = DB::table('material_quantities')->whereIn('component_item_id',$component_item_ids)
         ->join('material_items','material_quantities.material_item_id','=','material_items.id')
         ->get();
