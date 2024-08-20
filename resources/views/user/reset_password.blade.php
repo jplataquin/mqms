90a124daf12670f7012fc726dd5a843bdeac2a06
jplataquin <jp.lataquin@gmail.com>
2024-08-20 17:34:36 +0800

    fix

--- resources/views/user/reset_password.blade.php
+++ resources/views/user/reset_password.blade.php
@@ -29,13 +29,48 @@
 
                 <div class="row">
                     <div class="col-12 text-end">
-                        <button class="btn btn-secondary">Logout</button>
-                        <button class="btn btn-primary">Submit</button>
+                        
+                        <form id="logoutForm" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                        <button class="btn btn-secondary" id="logoutBtn" >Logout</button>
+                        <button class="btn btn-primary" id="submitBtn">Submit</button>
                     </div>
                 </div>
         </div>
     </div>
     <script type="module">
+        import {$q} from '/adarna.js';
+
+        const password      = $q('#password').first();
+        const repassword    = $q('#repassword').first();
+        const logoutBtn     = $q('#logoutBtn').first();
+        const submitBtn     = $q('#submitBtn').first();
+        const logoutForm    = $q('#logoutForm').first();
+        
+
+        submitBtn.onclick = ()=>{
+            window.util.blockUI();
+
+            window.util.$post('/api/user/reset_password',{
+                password    : password.value,
+                repassword  : repassword.value
+            }).then(reply=>{
+
+                window.util.unblockUI();
+
+                if(reply.status <= 0){
+                    window.util.showMsg(reply);
+                    return false;
+                }
+
+                document.location.href = '/home';
+            });
+        }
+
+        logoutBtn.onclick = (e)=>{
+            logoutForm.submit();
+        }
     </script>
 </div>
 @endsection
