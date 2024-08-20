00fbea79296815e2165c0194620724b61ad8f0ca
jplataquin <jp.lataquin@gmail.com>
2024-08-20 14:41:35 +0800

    fix

--- resources/views/material_canvass/print.blade.php
+++ resources/views/material_canvass/print.blade.php
@@ -40,7 +40,7 @@
     <table class="mb-20">
         <tr>
             <td style="width:60%;text-align:center">
-                <img src="storage/sys_images/header.png" style="width:500px"/>
+                <img src="storage/sys_images/header.png" style="width:400px"/>
             </td>
             <td class="text-center" style="width:30%;text-align:center">
                 <h1>Material Canvass</h1>
