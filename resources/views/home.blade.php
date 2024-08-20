4e8a72b3868c697761805183fa704ed0bc282c81
jplataquin <jp.lataquin@gmail.com>
2024-08-21 01:51:29 +0800

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,42 +4,36 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body text-center">
+                   
+            <div class="d-flex flex-wrap justify-content-evenly">
+                
+                <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
+                    <h5>Component</h5>
                     
-                    <div class="d-flex flex-wrap justify-content-evenly">
-                        
-                        <div class="text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
-                            <h5>Component</h5>
-                            
-                            <h3>{{$componentPendCount}}</h3>
-                            
-                            <h5>Pending</h5>
-                        </div>
-                        <div class="text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
-                            <h5>Material Request</h5>    
-                            <h3>{{$materialQuantityRequestPendCount}}</h3>
-                            <h5>Pending</h5>    
-                        </div>
-
-                        <div class="text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
-                            <h5>Material Canvass</h5>    
-                            <h3>{{$materialCanvassPendCount}}</h3>
-                            <h5>Pending</h5>    
-                        </div>
-
-
-                        <div class="text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
-                            <h5>Purchase Order</h5>    
-                            <h3>{{$purchaseOrderPendCount}}</h3>
-                            <h5>Pending</h5>    
-                        </div>
-
-                    </div>
+                    <h3>{{$componentPendCount}}</h3>
+                    
+                    <h5>Pending</h5>
                 </div>
+                <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
+                    <h5>Material Request</h5>    
+                    <h3>{{$materialQuantityRequestPendCount}}</h3>
+                    <h5>Pending</h5>    
+                </div>
+
+                <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
+                    <h5>Material Canvass</h5>    
+                    <h3>{{$materialCanvassPendCount}}</h3>
+                    <h5>Pending</h5>    
+                </div>
+
+                <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
+                    <h5>Purchase Order</h5>    
+                    <h3>{{$purchaseOrderPendCount}}</h3>
+                    <h5>Pending</h5>    
+                </div>
+
             </div>
+                
         </div>
     </div>
 </div>
