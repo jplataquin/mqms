ddb1b0a8448b786e1eb0b6dd9633a94668baef90
jplataquin <jp.lataquin@gmail.com>
2024-08-20 15:16:43 +0800

    fix

--- resources/views/material_canvass/print.blade.php
+++ resources/views/material_canvass/print.blade.php
@@ -117,9 +117,9 @@
                 </td>
             </tr>
             <tr>
-                <th style="width:20%;text-align:center">Status</th>
-                <th style="width:10%;text-align:center">Supplier</th>
-                <th style="width:10%;text-align:center">Payment Terms</th>
+                <th style="width:10%;text-align:center">Status</th>
+                <th style="width:20%;text-align:center">Supplier</th>
+                <th style="width:20%;text-align:center">Payment Terms</th>
                 <th style="width:10%;text-align:center">Price</th>
                 <th style="width:10%;text-align:center">Total</th>
             </tr>             
