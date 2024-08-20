20c281d95d1b73f3906fd8f5c2ef8ddd0160ebbd
jplataquin <jp.lataquin@gmail.com>
2024-08-20 15:15:53 +0800

    fix

--- resources/views/material_canvass/print.blade.php
+++ resources/views/material_canvass/print.blade.php
@@ -117,11 +117,11 @@
                 </td>
             </tr>
             <tr>
-                <th style="width:20%;text-align:center">Status</th>
-                <th style="width:20%;text-align:center">Supplier</th>
-                <th style="width:20%;text-align:center">Payment Terms</th>
-                <th style="width:20%;text-align:center">Price</th>
-                <th style="width:20%;text-align:center">Total</th>
+                <th style="width:10%;text-align:center">Status</th>
+                <th style="width:10%;text-align:center">Supplier</th>
+                <th style="width:10%;text-align:center">Payment Terms</th>
+                <th style="width:10%;text-align:center">Price</th>
+                <th style="width:10%;text-align:center">Total</th>
             </tr>             
             
             @foreach($item->MaterialCanvass as $mcItem)
