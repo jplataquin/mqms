d67613c77919814cc5079ab1b06551a83a0e4114
jplataquin <jp.lataquin@gmail.com>
2024-08-20 15:33:09 +0800

    fix

--- resources/views/review/material_canvass/list.blade.php
+++ resources/views/review/material_canvass/list.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div id="content">
 <div class="container">
-<div class="breadcrumbs">
+<div class="breadcrumbs" hx-boost="true" hx-select="#content" hx-target="#main">
         <ul>
             <li>
-                <a href="#">
+                <a href="/review/material_canvass">
                     <span>
                        Review
                     </span>
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="/review/material_canvass">
                     <span>
                        Material Canvass
                     </span>
@@ -24,69 +24,75 @@
                     <span>
                         List
                     </span>		
+                    <i class="ms-2 bi bi-list-ul"></i>
                 </a>
             </li>
         </ul>
     </div>
 <hr>
-
-<div class="row">
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label>Sort By</label>
-                <select class="form-control" id="sortSelect">
-                    <option value="1" selected>Latest Entry</option>
-                    <option value="2">Oldest Entry</option>
-                </select>
-            </div>
+    <div class="folder-form-container">
+        <div class="folder-form-tab">
+            Material Canvass Review
         </div>
-        <div class="col-lg-6">
-            <div class="form-group">
-                <label>ID</label>
-                <input type="text" id="query" class="form-control"/>
+        <div class="folder-form-body">
+            <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label>Sort By</label>
+                            <select class="form-control" id="sortSelect">
+                                <option value="1" selected>Latest Entry</option>
+                                <option value="2">Oldest Entry</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label>ID</label>
+                            <input type="text" id="query" class="form-control"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-lg-4">
+                        <div class="form-group">
+                            <label>Project</label>
+                            <select class="form-control" id="projectSelect">
+                                <option value=""> - </option>
+                                @foreach($projects as $project)
+                                    <option value="{{$project->id}}">{{$project->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                
+                    <div class="col-lg-4">
+                        <div class="form-group">
+                            <label>Section</label>
+                            <select class="form-control" id="sectionSelect">
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="form-group">
+                            <label>Component</label>
+                            <select class="form-control" id="componentSelect">
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-5">
+                    <div class="col-lg-8"></div>
+                    <div class="col-lg-2">
+                        <button id="searchBtn" class="btn w-100 btn-primary">Search</button>
+                    </div>
+                    <div class="col-lg-2">
+                        <button id="cancelBtn" class="btn w-100 btn-secondary">Cancel</button>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
-    <div class="row mb-3">
-        <div class="col-lg-4">
-            <div class="form-group">
-                <label>Project</label>
-                <select class="form-control" id="projectSelect">
-                    <option value=""> - </option>
-                    @foreach($projects as $project)
-                        <option value="{{$project->id}}">{{$project->name}}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-       
-        <div class="col-lg-4">
-            <div class="form-group">
-                <label>Section</label>
-                <select class="form-control" id="sectionSelect">
-                </select>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="form-group">
-                <label>Component</label>
-                <select class="form-control" id="componentSelect">
-                </select>
-            </div>
-        </div>
-    </div>
-    <div class="row mb-5">
-        <div class="col-lg-8"></div>
-        <div class="col-lg-2">
-            <button id="searchBtn" class="btn w-100 btn-primary">Search</button>
-        </div>
-        <div class="col-lg-2">
-            <button id="cancelBtn" class="btn w-100 btn-secondary">Cancel</button>
-        </div>
-    </div>
-
-</div>
-
     <div class="container">
         <table class="table border">
             <thead>
