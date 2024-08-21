eb2177b9710dc40eaa9ad6c996a048754238b8e9
jplataquin <jp.lataquin@gmail.com>
2024-08-21 18:24:09 +0800

    fix

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -185,7 +185,6 @@ class PurchaseOrderController extends Controller
                 }
             }
 
-            $mr->id = $available_item_count;
             //Include only request with remaining po'able items
             if($available_item_count > 0){
                 $results_to_show[] = $mr;
