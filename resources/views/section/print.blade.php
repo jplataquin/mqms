33aaee069745c0a7c08e7a2f2a83b4e7b3022a2e
jplataquin <jp.lataquin@gmail.com>
2024-08-23 23:53:50 +0800

    fix

--- resources/views/section/print.blade.php
+++ resources/views/section/print.blade.php
@@ -342,7 +342,7 @@
                         <tr class="@if(!$component_item->sum_flag || ($component_item->unit_id != $component->unit_id) ) bg-excluded-sum-component_item @endif">
                             
                             <td>
-                                {{$item_count}} {{$component_item->name}}
+                                {{$item_count}}.) {{$component_item->name}}
 
                                 @php
                                     $item_count++;
