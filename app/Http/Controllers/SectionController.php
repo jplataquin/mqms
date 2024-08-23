d1383867b4eaa71542c39b781f82ea4ffb390656
jplataquin <jp.lataquin@gmail.com>
2024-08-23 17:20:27 +0800

    fix

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -83,7 +83,7 @@ class SectionController extends Controller
         try {
             $html2pdf->setDefaultFont("Arial");
             $html2pdf->writeHTML($html);
-            $html2pdf->output('Purchase Order - '.str_pad($section->id,0,6,STR_PAD_LEFT ).'.pdf');
+            $html2pdf->output('Material Budget - '.str_pad($section->id,0,6,STR_PAD_LEFT ).'.pdf');
             $html2pdf->clean();
         
         }catch(Html2PdfException $e) {
