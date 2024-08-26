82e670a3095dafc8fe12363719f6585bc6f0cb63
jplataquin <jp.lataquin@gmail.com>
2024-08-26 17:00:08 +0800

    fix

--- resources/views/material_canvass/display.blade.php
+++ resources/views/material_canvass/display.blade.php
@@ -98,7 +98,7 @@
 
             <div class="row">
                 <div class="col-12 canvass-container" id="canvass_{{$item->id}}">
-                    <div class="text-center bg-secondary">
+                    <div class="text-center bg-divider">
                         <h5>Canvass Items</h5>
                     </div>
                 </div>
