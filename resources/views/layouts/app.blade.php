2f778714ca6102d741822388468d1eb9ac18c9ff
jplataquin <jp.lataquin@gmail.com>
2024-08-26 13:02:04 +0800

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -270,7 +270,7 @@
         htmx.on("htmx:afterOnLoad", function (event) {
             const xhr = event.detail;
             
-            console.log(xhr);
+            console.log(xhr.status);
             
             if(xhr.status == 403){  
                 console.log('here');
