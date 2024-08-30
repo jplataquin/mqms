cb8f4cc80bb7aa278f08622c1f122efeb2f52853
jplataquin <jp.lataquin@gmail.com>
2024-08-30 13:10:42 +0800

    fix

--- resources/views/material_canvass/print.blade.php
+++ resources/views/material_canvass/print.blade.php
@@ -118,7 +118,7 @@
                     <th style="background-color:#d3d3d3;text-align:center; width:20%">
                         Quantity
                     </th>
-                    <th style="background-color:#d3d3d3;text-align:center">
+                    <th style="background-color:#d3d3d3;text-align:center; width:30%">
                         Budget Price
                     </th>
                 </tr>
