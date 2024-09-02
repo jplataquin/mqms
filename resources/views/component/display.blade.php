c307bc4b4fa0d55a795ae72d1f90abf6346df427
jplataquin <jp.lataquin@gmail.com>
2024-09-03 02:00:07 +0800

    fix

--- resources/views/component/display.blade.php
+++ resources/views/component/display.blade.php
@@ -292,8 +292,7 @@
             if (!entry.isIntersecting) {
                 component_controls.style.display = 'none';
             }else{
-                console.log(component_controls.style.display);
-                //component_controls.style.display = 'inline';
+                component_controls.style.display = '';
             }
         }
     });
