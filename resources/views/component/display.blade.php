f031fd8282c0c39c1eea6eb9ac6857482b8caa7e
jplataquin <jp.lataquin@gmail.com>
2024-09-05 15:21:56 +0800

    fix

--- resources/views/component/display.blade.php
+++ resources/views/component/display.blade.php
@@ -116,7 +116,7 @@
             <div class="d-none form-container" id="item_sticky_container">
                 <div class="form-header">Item</div>
                 <div class="form-body">
-                    <div class="row">
+                    <div class="row mb-3">
                         
                         <div class="col-lg-8">
                             <div class="form-group">
@@ -376,85 +376,44 @@
 
     sticky_observer.observe(component_sticky_trigger);
 
-    
-    // $q('.component_item_sticky_untrigger').items().map(item=>{
-
-                    
-    //     let o = new IntersectionObserver((entries)=>{
-    //         for (let entry of entries) {
-
-    //             // Check if the element is intersecting the viewport
-    //             if (!entry.isIntersecting) {
-    //                 component_form.style.display = 'none';
-    //             }else{
-    //                 component_controls.style.display = '';
-    //                 item_sticky_container.classList.add('d-none');
-    //             }
-    //         }
-    //     });
-
-    //     o.observe(item);
-    // });
-    // let sticky_display_trigger = false;
-    //let past_untrigger = null;
 
     window.addEventListener("scroll", window.util.throttle((event) => {
 
 
-            // if(sticky_display_trigger == false){
-                // $q('.component_item_sticky_untrigger').items().map(item=>{
+        $q('.component_item_sticky_trigger').items().map(item=>{
+            
+            
+            let item_pos = item.getBoundingClientRect().top + document.documentElement.scrollTop;
+            let form_pos = component_form.getBoundingClientRect().top + document.documentElement.scrollTop +  component_form.offsetHeight;
+            
 
-                    
-                //     let item_pos = item.getBoundingClientRect().top + document.documentElement.scrollTop;
-                //     let form_pos = component_form.getBoundingClientRect().top + document.documentElement.scrollTop +  component_form.offsetHeight;
+            if(form_pos >= item_pos){
 
+                let parent = item.parentElement;
+            
+                let name            = parent.querySelector('.name');
+                let sum_flag        = parent.querySelector('.sum_flag');
+                let function_type   = parent.querySelector('.function_type');
+                let variable        = parent.querySelector('.variable');
+                let quantity        = parent.querySelector('.quantity');
+                let equivalent      = parent.querySelector('.equivalent');
+                let unit            = parent.querySelector('.unit');
+                let budget_price    = parent.querySelector('.budget_price');
+                
+                item_name.value             = name.value;
+                item_sum_flag.checked       = sum_flag.checked;
+                item_function_type.value    = function_type.options[function_type.selectedIndex].innerText;
+                item_variable.value         = variable.value;
+                item_quantity.value         = quantity.value;
+                item_equivalent.value       = equivalent.value;
+                item_unit.value             = unit.value;
+                item_budget_price.value     = budget_price.value;
+                
+                
+                item_sticky_container.classList.remove('d-none');
 
-                //     if(form_pos >= item_pos && past_untrigger != item){
-                //         item_sticky_container.classList.add('d-none');
-                //         past_untrigger = item;
-                //     }
-                // });
-
-            // }else{
-
-
-                $q('.component_item_sticky_trigger').items().map(item=>{
-                    
-                    
-                    let item_pos = item.getBoundingClientRect().top + document.documentElement.scrollTop;
-                    let form_pos = component_form.getBoundingClientRect().top + document.documentElement.scrollTop +  component_form.offsetHeight;
-                    
-
-                    if(form_pos >= item_pos){
-
-                        let parent = item.parentElement;
-                    
-                        let name            = parent.querySelector('.name');
-                        let sum_flag        = parent.querySelector('.sum_flag');
-                        let function_type   = parent.querySelector('.function_type');
-                        let variable        = parent.querySelector('.variable');
-                        let quantity        = parent.querySelector('.quantity');
-                        let equivalent      = parent.querySelector('.equivalent');
-                        let unit            = parent.querySelector('.unit');
-                        let budget_price    = parent.querySelector('.budget_price');
-                        
-                        item_name.value             = name.value;
-                        item_sum_flag.checked       = sum_flag.checked;
-                        item_function_type.value    = function_type.options[function_type.selectedIndex].innerText;
-                        item_variable.value         = variable.value;
-                        item_quantity.value         = quantity.value;
-                        item_equivalent.value       = equivalent.value;
-                        item_unit.value             = unit.value;
-                        item_budget_price.value     = budget_price.value;
-                        
-                        
-                        item_sticky_container.classList.remove('d-none');
-
-                    }
-                });
-
-          //  }//if else
-
+            }
+        });
 
     },300));
 
