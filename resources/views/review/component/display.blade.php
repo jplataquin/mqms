7490a74614f23cf42592c93a058ec39a2bdc4102
jplataquin <jp.lataquin@gmail.com>
2024-09-10 13:58:42 +0800

    fix

--- resources/views/review/component/display.blade.php
+++ resources/views/review/component/display.blade.php
@@ -100,81 +100,81 @@
             $grand_total = 0;
         @endphp
         @foreach($componentItems as $item)
+            <div class="form-container">
+                <div class="form-header">
+                    {{$i}}.) {{$item->name}}
+                </div>
+                <div class="form-body">
+                    <table border="1" class="table">
+                        
+                        <tr>
+                            <th class="text-center">Factor</th>
+                            <th class="text-center">Quantity</th>
+                            <th class="text-center">Budget Price</th>
+                            <th class="text-center">Total Amount</th>
+                        </tr>
+                        <tr>
+                            <td class="text-center">
+                                Php {{ number_format($item->budget_price,2) }}
+                            </td>
+                            <td class="text-center">
 
-        <div class="form-container">
-            <div class="form-title">
-                {{$i}}.) {{$item->name}}
-            </div>
-                <table border="1" class="table">
-                    
-                    <tr>
-                        <th class="text-center">Factor</th>
-                        <th class="text-center">Quantity</th>
-                        <th class="text-center">Budget Price</th>
-                        <th class="text-center">Total Amount</th>
-                    </tr>
-                    <tr>
-                        <td class="text-center">
-                            Php {{ number_format($item->budget_price,2) }}
-                        </td>
-                        <td class="text-center">
-
-                            @if($item->function_type_id == 1)
-                             {{$item->function_variable}} {{ $unit_options[ $item->unit_id ]->text }} / {{$unit_options[$component->unit_id]->text}}
-                            @elseif($item->function_type_id == 2)
-                             1 {{$unit_options[$component->unit_id]->text}} / {{$item->function_variable}} {{ $unit_options[ $item->unit_id ]->text }}
-                            @else
-                                N/A
-                            @endif
+                                @if($item->function_type_id == 1)
+                                {{$item->function_variable}} {{ $unit_options[ $item->unit_id ]->text }} / {{$unit_options[$component->unit_id]->text}}
+                                @elseif($item->function_type_id == 2)
+                                1 {{$unit_options[$component->unit_id]->text}} / {{$item->function_variable}} {{ $unit_options[ $item->unit_id ]->text }}
+                                @else
+                                    N/A
+                                @endif
+                                
+                                <br>
+                                _________
+                                <br>
+                                {{$component->use_count}} Use(s)
+                            </td>
+                            <td class="text-center">
+                                {{$item->quantity}} {{$unit_options[$item->unit_id]->text}}
+                            </td>
+                            <td class="text-center">
+                                @php
+                                    $grand_total = $grand_total + ($item->budget_price * $item->quantity);
+                                @endphp
+                                Php {{ number_format($item->budget_price * $item->quantity,2) }}
+                            </td>
+                        </tr>
+                    </table>
+                    <table border="1" class="table">
                             
-                            <br>
-                            _________
-                            <br>
-                            {{$component->use_count}} Use(s)
-                        </td>
-                        <td class="text-center">
-                            {{$item->quantity}} {{$unit_options[$item->unit_id]->text}}
-                        </td>
-                        <td class="text-center">
-                            @php
-                                $grand_total = $grand_total + ($item->budget_price * $item->quantity);
-                            @endphp
-                            Php {{ number_format($item->budget_price * $item->quantity,2) }}
-                        </td>
-                    </tr>
-                </table>
-                <table border="1" class="table">
-                        
 
-                        <tr>
-                            <th style="width:40%" class="text-center">Material</th>
-                            <th style="width:20%" class="text-center">Equivalent</th>
-                            <th style="width:20%" class="text-center">Quantity</th>
-                            <th style="width:20%" class="text-center">Total</th>
-                        </tr>
-                        
-                        @foreach($item->materialQuantities as $mq)
-                        <tr>
-                            <td>
-                                {{$materialItems[$mq->material_item_id]->name }} 
-                                {{$materialItems[$mq->material_item_id]->specification_unit_packaging }} 
-                                {{$materialItems[$mq->material_item_id]->brand }} 
-                            </td>
-                            <td class="text-center">
-                                {{$mq->equivalent}} {{$item->unit}}
-                            </td>
-                            <td class="text-center">
-                                {{$mq->quantity}}
-                            </td>
-                            <td class="text-center">
-                                {{$mq->equivalent * $mq->quantity}} {{$item->unit}}
-                            </td>
-                        </tr>
-                        @endforeach
+                            <tr>
+                                <th style="width:40%" class="text-center">Material</th>
+                                <th style="width:20%" class="text-center">Equivalent</th>
+                                <th style="width:20%" class="text-center">Quantity</th>
+                                <th style="width:20%" class="text-center">Total</th>
+                            </tr>
+                            
+                            @foreach($item->materialQuantities as $mq)
+                            <tr>
+                                <td>
+                                    {{$materialItems[$mq->material_item_id]->name }} 
+                                    {{$materialItems[$mq->material_item_id]->specification_unit_packaging }} 
+                                    {{$materialItems[$mq->material_item_id]->brand }} 
+                                </td>
+                                <td class="text-center">
+                                    {{$mq->equivalent}} {{$item->unit}}
+                                </td>
+                                <td class="text-center">
+                                    {{$mq->quantity}}
+                                </td>
+                                <td class="text-center">
+                                    {{$mq->equivalent * $mq->quantity}} {{$item->unit}}
+                                </td>
+                            </tr>
+                            @endforeach
 
-                </table>
+                    </table>
+                </div>
             </div>
-        
         @php $i++ @endphp
         @endforeach
     
