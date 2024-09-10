70b345d294bd2e9d1e1464fa64946f98d8423811
jplataquin <jp.lataquin@gmail.com>
2024-09-10 15:01:47 +0800

    fix

--- resources/views/review/component/display.blade.php
+++ resources/views/review/component/display.blade.php
@@ -64,8 +64,9 @@
                                 </div>
                             </div>
                         </div>
+                        
                     </div>
-                    <div class="col-lg-6">\
+                    <div class="col-lg-6">
                         <div class="form-container">
                             <div class="form-header">POW/DUPA</div>
                             <div class="form-body">
