e360693c69d86fe090c7a6dfe44ce787dc06374e
jplataquin <jp.lataquin@gmail.com>
2024-09-13 15:43:53 +0800

    fix

--- resources/views/review/component/display.blade.php
+++ resources/views/review/component/display.blade.php
@@ -221,7 +221,7 @@
                                                     @endif
                                                 @endforeach
                                             </select>
-                                            <span class="input-group-text" id="basic-addon2" onclick="document.querySelector('#component').focus();"><i class="bi bi-search"></i></span>
+                                            <span class="input-group-text" id="basic-addon2" onclick="document.querySelector('#component').focus();"><i class="bi bi-list"></i></span>
                                             </div>
                                         </div>
                                     </div>
