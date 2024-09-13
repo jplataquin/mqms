69be324e2a011cc15c410299ed2c016e22a519a6
jplataquin <jp.lataquin@gmail.com>
2024-09-13 16:14:43 +0800

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -269,7 +269,7 @@
 
             const xhr = event.detail;
             
-            console.log(event);
+            console.log(event.detail);
             if (xhr.status == 404) {
                 event.stopPropagation(); // Tell htmx not to process these requests
                 window.util.alert('Error','Page not found (404)');
