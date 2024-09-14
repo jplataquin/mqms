a2adeff8c12b75751f31f86a1018076539fea0ff
jplataquin <jp.lataquin@gmail.com>
2024-09-14 18:45:52 +0800

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-                   
-            <div class="d-flex flex-wrap justify-content-evenly">
-                
-                <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
-                    <h5>Component</h5>
+<div id="content">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
                     
-                    <h3>{{$componentPendCount}}</h3>
+                <div class="d-flex flex-wrap justify-content-evenly">
                     
-                    <h5>Pending</h5>
-                </div>
-                <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
-                    <h5>Material Request</h5>    
-                    <h3>{{$materialQuantityRequestPendCount}}</h3>
-                    <h5>Pending</h5>    
-                </div>
+                    <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
+                        <h5>Component</h5>
+                        
+                        <h3>{{$componentPendCount}}</h3>
+                        
+                        <h5>Pending</h5>
+                    </div>
+                    <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
+                        <h5>Material Request</h5>    
+                        <h3>{{$materialQuantityRequestPendCount}}</h3>
+                        <h5>Pending</h5>    
+                    </div>
 
-                <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
-                    <h5>Material Canvass</h5>    
-                    <h3>{{$materialCanvassPendCount}}</h3>
-                    <h5>Pending</h5>    
-                </div>
+                    <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
+                        <h5>Material Canvass</h5>    
+                        <h3>{{$materialCanvassPendCount}}</h3>
+                        <h5>Pending</h5>    
+                    </div>
 
-                <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
-                    <h5>Purchase Order</h5>    
-                    <h3>{{$purchaseOrderPendCount}}</h3>
-                    <h5>Pending</h5>    
-                </div>
+                    <div class="shadow-lg text-center m-3 rounded border border-primary p-5 rounded" style="width:300px">
+                        <h5>Purchase Order</h5>    
+                        <h3>{{$purchaseOrderPendCount}}</h3>
+                        <h5>Pending</h5>    
+                    </div>
 
+                </div>
+                    
             </div>
-                
         </div>
     </div>
 </div>
