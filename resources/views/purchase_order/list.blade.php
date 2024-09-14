fc9ea17c010c8503226cd7a0ea187e4c31fa6cf6
jplataquin <jp.lataquin@gmail.com>
2024-09-14 16:37:27 +0800

    fix

--- resources/views/purchase_order/list.blade.php
+++ resources/views/purchase_order/list.blade.php
@@ -96,31 +96,17 @@
                 </div>
 
                 <div class="row">
-                        <div class="col-lg-8"></div>
-                        <div class="col-lg-2">
-                            <div class="form-group">
-                                <label>&nbsp;</label>
-                                <button id="searchBtn" class="btn w-100 btn-primary">Search</button>
-                            </div>
-                        </div>
-                        <div class="col-lg-2">
-                            <div class="form-group">
-                                <label>&nbsp;</label>
-                                <button id="cancelBtn" class="btn w-100 btn-secondary">Cancel</button>
-                            </div>
+                        <div class="col-lg-12 text-end">    
+                            
+                                <button id="searchBtn" class="btn btn-primary">Search</button>
+                                
+                                <button id="createBtn" class="btn btn-warning">Create</button>
+
+                                <button id="cancelBtn" class="btn btn-secondary">Cancel</button>
+                    
                         </div> 
                 </div>
 
-                <div class="row mb-3">
-                        <div class="col-lg-8"></div>
-                        
-                        <div class="col-lg-4">
-                            <div class="form-group">
-                                <label>&nbsp;</label>
-                                <button id="createBtn" class="btn w-100 btn-warning">Create</button>
-                            </div>
-                        </div> 
-                </div>
             </div>
     </div>
 
