2343df8307a46d0759b06555206d3d903e4199a3
jplataquin <jp.lataquin@gmail.com>
2024-09-17 17:33:08 +0800

    fix

--- resources/views/review/material_canvass/list.blade.php
+++ resources/views/review/material_canvass/list.blade.php
@@ -114,20 +114,27 @@
 <script type="module">
     import {$q,Template,$el,$util} from '/adarna.js';
 
-    let list            = $q('#list').first();
-    let query           = $q('#query').first();
-    let projectSelect   = $q('#projectSelect').first();
-    let sectionSelect   = $q('#sectionSelect').first();
-    let componentSelect = $q('#componentSelect').first();
+    const list            = $q('#list').first();
+    const query           = $q('#query').first();
+    const projectSelect   = $q('#projectSelect').first();
+    const sectionSelect   = $q('#sectionSelect').first();
+    const componentSelect = $q('#componentSelect').first();
  
-    let searchBtn       = $q('#searchBtn').first();
-    let showMoreBtn     = $q('#showMoreBtn').first();
-    let sortSelect      = $q('#sortSelect').first();
-    let cancelBtn       = $q('#cancelBtn').first();
-    let createBtn       = $q('#createBtn').first();
+    const searchBtn       = $q('#searchBtn').first();
+    const showMoreBtn     = $q('#showMoreBtn').first();
+    const sortSelect      = $q('#sortSelect').first();
+    const cancelBtn       = $q('#cancelBtn').first();
+    const createBtn       = $q('#createBtn').first();
+    
     let page            = 1;
     let order           = 'DESC';
     let orderBy         = 'id';
+
+
+    window.util.quickNav = {
+        title:'Review Material Canvass',
+        url: '/review/material_canvass'
+    };
 
     const t = new Template();
 
