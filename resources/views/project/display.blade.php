fb5550ea5ce2ba75f1156d9fd6cf3835fddc50ee
jplataquin <jp.lataquin@gmail.com>
2024-09-18 17:52:24 +0800

    fix

--- resources/views/project/display.blade.php
+++ resources/views/project/display.blade.php
@@ -58,13 +58,13 @@
             </div>
 
             <div class="row mt-5 mb-3">
-                <div class="col-6 text-start">
+                <div class="col-lg-12 text-end">
                     <button class="btn btn-danger" id="deleteBtn">Delete</button>
-                </div>
-                <div class="col-6 text-end">
-                    <button class="btn btn-secondary" id="cancelBtn">Cancel</button>
+          
                     <button class="btn btn-primary" id="editBtn">Edit</button>
                     <button class="btn btn-warning d-none" id="updateBtn">Update</button>
+                    <button class="btn btn-secondary" id="cancelBtn">Cancel</button>
+          
                 </div>
             </div>
         </div>
