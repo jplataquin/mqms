7813b9cc6c62f294ba31780b7f46ab05c783c202
jplataquin <jp.lataquin@gmail.com>
2024-09-20 13:43:32 +0800

    fix

--- resources/views/review/component/display.blade.php
+++ resources/views/review/component/display.blade.php
@@ -215,12 +215,12 @@
                 <div class="row mb-3">
                     <div class="col-lg-12">
                         <div class="form-container">
-                            <div class="form-header @if(!$component->sum_flag || $component->unit_id != $contract_item->contract_unit_id) bg-excluded-sum-component @endif">Component</div>
+                            <div class="form-header @if(!$component->sum_flag || $component->unit_id != $contract_item->unit_id) bg-excluded-sum-component @endif">Component</div>
                             <div class="form-body">
                                 <div class="row mb-3">
                                     <div class="col-lg-12">
                                         <div class="form-group">
-                                            <label>Name {{$component->unit_id}} | {{$contract_item->contract_unit_id}}</label>
+                                            <label>Name {{$component->unit_id}} | {{$contract_item->unit_id}}</label>
                                             <div class="input-group">
                                                 <span class="input-group-text" id="basic-addon2" onclick="document.querySelector('#component').focus();"><i class="bi bi-list"></i></span>
                                             
