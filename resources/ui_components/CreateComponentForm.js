0ad8e1a55a54a00bcea30e9bc8739fb8d6280b4d
jplataquin <jp.lataquin@gmail.com>
2024-09-26 17:02:57 +0800

    fix

--- resources/ui_components/CreateComponentForm.js
+++ resources/ui_components/CreateComponentForm.js
@@ -61,7 +61,7 @@ class CreateComponentForm extends Component{
 
             t.div({class:'row mb-3'},()=>{
                 t.div({class:'col-lg-12 text-end'},()=>{
-                    this.el.submt_btn   = t.div({class:'btn btn-primary'},'Submit');
+                    this.el.submit_btn   = t.div({class:'btn btn-primary'},'Submit');
                     this.el.cancel_btn  = t.div({class:'btn btn-secondary'},'Cancel');
                 });
             });//div row
@@ -138,6 +138,7 @@ class CreateComponentForm extends Component{
 
             //Component(reply.data.id)).to(component_list)
             this._model.callback(reply.data.id);
+            window.util.drawerModal.close();
         });
 
     }
