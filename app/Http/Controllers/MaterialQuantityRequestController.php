a6cc8a5cf9a2cf851988a444d4b08398d21df6b5
jplataquin <jp.lataquin@gmail.com>
2024-09-27 14:17:53 +0800

    fix

--- app/Http/Controllers/MaterialQuantityRequestController.php
+++ app/Http/Controllers/MaterialQuantityRequestController.php
@@ -415,7 +415,7 @@ class MaterialQuantityRequestController extends Controller
             'material_quantities.material_item_id AS material_item_id',
             'material_items.name AS name',
             'material_items.specification_unit_packaging AS specification_unit_packaging',
-            'material.items.brand AS brand',
+            'material_items.brand AS brand',
             'material_quantities.quantity AS quantity',
             'material_quantities.deleted_flag AS deleted_flag'
         )
