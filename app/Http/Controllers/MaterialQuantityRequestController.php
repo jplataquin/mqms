14b8ccba23cc08afe3423840dabedaf8539f3d43
jplataquin <jp.lataquin@gmail.com>
2024-09-27 14:17:34 +0800

    fix

--- app/Http/Controllers/MaterialQuantityRequestController.php
+++ app/Http/Controllers/MaterialQuantityRequestController.php
@@ -411,6 +411,14 @@ class MaterialQuantityRequestController extends Controller
         
         $material_item_result = DB::table('material_quantities')->whereIn('component_item_id',$component_item_ids)
         ->join('material_items','material_quantities.material_item_id','=','material_items.id')
+        ->select(
+            'material_quantities.material_item_id AS material_item_id',
+            'material_items.name AS name',
+            'material_items.specification_unit_packaging AS specification_unit_packaging',
+            'material.items.brand AS brand',
+            'material_quantities.quantity AS quantity',
+            'material_quantities.deleted_flag AS deleted_flag'
+        )
         ->get();
 
         $material_options = [];
