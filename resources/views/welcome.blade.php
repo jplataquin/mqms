ea72e6c89e8307a24277ab6f8264588a51a470df
jplataquin <jp.lataquin@gmail.com>
2024-10-03 18:59:51 +0800

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,8 +39,9 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 
 
-                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                <div class="flex justify-center mt-4">
                     <h1>Aeon</h1>
+                    
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
