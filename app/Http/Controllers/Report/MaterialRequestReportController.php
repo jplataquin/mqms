2ff3f74eb1024368a2272111166a4312512f9078
jplataquin <jp.lataquin@gmail.com>
2024-10-06 16:39:39 +0800

    fix

--- app/Http/Controllers/Report/MaterialRequestReportController.php
+++ app/Http/Controllers/Report/MaterialRequestReportController.php
@@ -11,7 +11,7 @@ use App\Models\Component;
 use App\Models\Supplier;
 use App\Models\User;
 use App\Models\MaterialQuantityRequestItem;
-use App\Models\MaterialQuantity;
+use App\Models\MaterialQuantityRequest;
 use App\Models\PurchaseOrderItem;
 use App\Models\MaterialItem;
 use App\Models\ComponentUnit;
@@ -36,8 +36,44 @@ class MaterialRequestReportController extends Controller
     }
 
     public function generate(Request $request){
+
+        $project_id         = $request->input('project_id');
+        $section_id         = $request->input('section_id');
+        $contract_item_id   = $request->input('contract_item_id');
+        $component_id       = $request->input('component_id');
+        $from               = $request->input('from');
+        $to                 = $request->input('to');
+        $requested_by       = $request->input('requested_by');
+        $status             = $request->input('status');
+
+        //Query material request
+        $material_request = new MaterialQuantityRequst();
+
+        if($project_id){
+
+            $project_id = (int) $project_id;
+            $material_request = $material_request->where('project_id',$project_id);
+
+            if($section_id){
+                $section_id = (int) $section_id;
+                $material_request = $material_request->where('section_id',$section_id);
+
+                if($contract_item_id){
+                    $contract_item_id = (int) $contract_item_id;
+                    $material_request = $material_request->where('contract_item_id',$contract_item_id);
+                    
+                    if($component_id){
+                        $component_id = (int) $component_id;
+                        $material_request = $material_request->where('component_id',$component_id);
+                        
+                    }            
+                }   
+            }
+        }/*****/
+
+
+        
         $material_item_id_arr = explode(',',$request->input('material_items'));
 
-        print_r($material_item_id_arr);
     }
 }
