fcdf15cfec5399ec0a1a174f89cbe8e1c8f7d902
jplataquin <jp.lataquin@gmail.com>
2024-10-08 16:02:34 +0800

    fix

--- app/Http/Controllers/Report/MaterialItemReportController.php
+++ app/Http/Controllers/Report/MaterialItemReportController.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\DB;
 class MaterialItemReportController extends Controller
 {
 
-    public function paramters(){
+    public function parameters(){
 
         $material_groups = MaterialGroup::where('deleted_at',null)->get();
 
