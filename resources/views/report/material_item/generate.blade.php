8ab26b4a0f6239531821d9f2877be2a5aaa04ca4
jplataquin <jp.lataquin@gmail.com>
2024-10-08 17:05:38 +0800

    fix

--- resources/views/report/material_item/generate.blade.php
+++ resources/views/report/material_item/generate.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts.app')
+
 @section('content')
 <div id="content">
     <div class="container">
@@ -22,7 +24,36 @@
         <hr>
 
 
-     
+        @foreach($result as $material_item_id => $res1)
+
+            <div class="mb-3 ps-3">
+                <h2>{{ $material_item_options[$material_item_id]->text }}</h2>
+
+                @foreach($res1 as $supplier_id => $res2)
+                    <div class="mb-3 ps-3">
+                        <h3>{{ $supplier_options[$supplier_id]->text }}</h3>
+
+                        @foreach($res2 as $payment_term_id => $res3)
+                            <div class="mb-3 ps-3">
+                                <h4>{{ $payment_term_options[$payment_term_id]->text }}</h4>
+
+                                <ul class="list-group">
+                                    @foreach($res3 as $price => $res4)
+                                        <li class="list-group-item">P {{number_format($price,2)}}</li>
+                                    @endforeach
+                                
+                                </ul>
+                            </div>
+                        @endforeach
+                    </div>
+
+                @endforeach
+
+            </div>
+
+        @endforeach
+    
+
     </div>
 
     <script type="module">
