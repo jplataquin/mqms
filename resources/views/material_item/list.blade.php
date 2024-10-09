14e83537a961a233bcd364a51f0693b21bde03f3
jplataquin <jp.lataquin@gmail.com>
2024-10-09 17:07:48 +0800

    fix

--- resources/views/material_item/list.blade.php
+++ resources/views/material_item/list.blade.php
@@ -76,12 +76,13 @@
 <script type="module">
     import {$q,Template,$el} from '/adarna.js';
 
-    let list            = $q('#list').first();
-    let query           = $q('#query').first();
-    let searchBtn       = $q('#searchBtn').first();
-    let showMoreBtn     = $q('#showMoreBtn').first();
-    let sortSelect      = $q('#sortSelect').first();
-    let createBtn       = $q('#createBtn').first();
+    const list            = $q('#list').first();
+    const query           = $q('#query').first();
+    const searchBtn       = $q('#searchBtn').first();
+    const showMoreBtn     = $q('#showMoreBtn').first();
+    const sortSelect      = $q('#sortSelect').first();
+    const createBtn       = $q('#createBtn').first();
+    
     let page            = 1;
     let order           = 'DESC';
     let orderBy         = 'id';
