7bed9c6de87f5e2bca1eaae7dec38e15827a713f
jplataquin <jp.lataquin@gmail.com>
2024-10-10 16:00:05 +0800

    fix

---
+++ database/migrations/2024_10_10_071907_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->char('comment_type',4);
+            $table->char('record_type',6);
+            $table->bigInteger('record_id');
+            $table->longText('content');
+            $table->bigInteger('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
