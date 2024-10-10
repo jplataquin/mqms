1b6ccecff63f986728a346a204d9e9cf4183d94b
jplataquin <jp.lataquin@gmail.com>
2024-10-10 14:33:26 +0800

    fix

--- resources/views/material_item/list.blade.php
+++ resources/views/material_item/list.blade.php
@@ -29,8 +29,8 @@
             Material Items
         </div>
         <div class="folder-form-body">
-            <div class="row">
-                <div class="col-lg-4">
+            <div class="row mb-3">
+                <div class="col-lg-6">
                     <div class="form-group">
                         <label>Sort By</label>
                         <select class="form-control" id="sortSelect">
@@ -41,13 +41,15 @@
                         </select>
                     </div>
                 </div>
-                <div class="col-lg-4">
+                <div class="col-lg-6">
                     <div class="form-group">
                         <label>Query</label>
                         <input type="text" id="query" class="form-control"/>
                     </div>
                 </div>
-                <div class="col-lg-4">
+            </div>
+            <div class="row">
+                <div class="col-lg-12 text-end">
                         <button id="searchBtn" class="btn btn-primary">Search</button>
                         <button id="createBtn" class="btn btn-warning">Create</button>
                         <button id="printBtn" class="btn btn-secondary">Print</button>
