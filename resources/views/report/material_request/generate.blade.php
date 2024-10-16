eab61d2a7fa997a664ceef3c7ebfdc8fd678a453
jplataquin <jp.lataquin@gmail.com>
2024-10-16 18:55:56 +0800

    fix

--- resources/views/report/material_request/generate.blade.php
+++ resources/views/report/material_request/generate.blade.php
@@ -8,17 +8,17 @@
             
 
                 <tr>
-                    <td colspan="2">{{ $contract_item_arr[$contract_item_id]->item_code }} {{$contract_item_arr[$contract_item_id]->description}}</td>
+                    <td colspan="2">1 {{ $contract_item_arr[$contract_item_id]->item_code }} {{$contract_item_arr[$contract_item_id]->description}}</td>
                 </tr>
 
                 @foreach($contract_item as $component_id => $component)
                     <tr>
-                        <td colspan="2">{{$component_arr[$component_id]->name}}</td>
+                        <td colspan="2">2 {{$component_arr[$component_id]->name}}</td>
                     </tr>
 
                     @foreach($component as $component_item_id => $component_item)
                         <tr>
-                            <td colspan="2">{{$component_item_arr[$component_item_id]->name}}</td>
+                            <td colspan="2">3 {{$component_item_arr[$component_item_id]->name}}</td>
                         </tr>
 
                         @foreach($component_item as $material_quantity_id => $result){
@@ -26,7 +26,7 @@
                                 @php 
                                     $material_item = $material_item_arr[ $material_quantity_arr[$material_quantity_id]->material_item_id ];
                                 @endphp
-                                <td colspan="2">{{ $material_item->formatted_name }}</td>
+                                <td colspan="2">4 {{ $material_item->formatted_name }}</td>
                             </tr>
                             <tr>
                                 <td>Budget</td>
