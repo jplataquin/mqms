0ea53d509b3ffd2991b588caa00a436842b0336a
jplataquin <jp.lataquin@gmail.com>
2024-10-17 16:37:18 +0800

    fix

--- resources/views/report/material_request/generate.blade.php
+++ resources/views/report/material_request/generate.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+
+    .contract_item{
+        background-color: #d6d4d0;
+    }
+
+    
+</style>
 <div id="content">
     <div class="container">
         <table class="table">
@@ -8,18 +16,18 @@
             
 
                 <tr>
-                    <td colspan="2">1 {{ $contract_item_arr[$contract_item_id]->item_code }} {{$contract_item_arr[$contract_item_id]->description}}</td>
+                    <th colspan="2" class="contract_item">{{ $contract_item_arr[$contract_item_id]->item_code }} {{$contract_item_arr[$contract_item_id]->description}}</th>
                 </tr>
                 
                 @foreach($contract_item as $component_id => $component)
                     
                     <tr>
-                        <td colspan="2" style="padding-left:3em" >2 {{ $component_arr[ $component_id ]->name }}</td>
+                        <td colspan="2" style="padding-left:3em" class="component">2 {{ $component_arr[ $component_id ]->name }}</td>
                     </tr>
 
                     @foreach($component as $component_item_id => $component_item)
                         <tr>
-                            <td colspan="2" style="padding-left:6em" >3 {{$component_item_arr[$component_item_id]->name}}</td>
+                            <td colspan="2" style="padding-left:6em" class="component_item">3 {{$component_item_arr[$component_item_id]->name}}</td>
                         </tr>
 
                         @foreach($component_item as $material_quantity_id => $result)
@@ -27,7 +35,7 @@
                                 @php 
                                     $material_item = $material_item_arr[ $material_quantity_arr[$material_quantity_id]->material_item_id ];
                                 @endphp
-                                <td colspan="2" style="padding-left:9em" >4 {{ $material_item->formatted_name() }}</td>
+                                <td colspan="2" style="padding-left:9em" class="material_item">4 {{ $material_item->formatted_name() }}</td>
                             </tr>
                             <tr>
                                 <td style="padding-left:12em">Budget</td>
