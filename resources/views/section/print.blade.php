a9c41f5fe8f63d39591567857ed3c14664ee8c07
jplataquin <jp.lataquin@gmail.com>
2024-10-17 18:03:35 +0800

    fix

--- resources/views/section/print.blade.php
+++ resources/views/section/print.blade.php
@@ -299,6 +299,8 @@
                     <tr class="@if(!$component->sum_flag || ($component->unit_id != $contract_item->unit_id)) bg-excluded-sum-component @endif">
                             @if($first)
                             <td rowspan="{{count($component_items_arr[$component->id])+2}}">
+                                [{{$component->id}}]
+                                <br>
                                 {!! Str::wordWrap($component->name,10,"<br>",false) !!}
                             </td>
                                 
