19f1ef44ff8ce97777fc28d317f7e56f55dba9b7
jplataquin <jp.lataquin@gmail.com>
2024-10-18 16:46:02 +0800

    fix

--- resources/views/report/project/generate.blade.php
+++ resources/views/report/project/generate.blade.php
@@ -80,7 +80,7 @@
                                 <td colspan="2" style="padding-left:3em" class="material_item">4 {{ $material_item->formatted_name() }}</td>
                             </tr>
                             <tr>
-                                <td style="padding-left:4em">
+                                <td style="padding-left:5em">
                                     Budget
                                     <div class="progress">
                                         <div class="progress-bar bg-success" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">100%</div>
@@ -89,7 +89,7 @@
                                 <td>{{ number_format($result['budget_quantity'],2) }} Qty</td>
                             </tr>
                             <tr>
-                                <td style="padding-left:4em">
+                                <td style="padding-left:5em">
                                     @php 
                                         if($result['request_quantity']){
                                             $request_percentage = ($result['request_quantity'] / $result['budget_quantity']) * 100;
@@ -109,7 +109,7 @@
                                 <td class="@if($result['request_quantity'] > $result['budget_quantity']) text-danger overbudget @endif">{{ number_format($result['request_quantity'],2) }} Qty</td>
                             </tr>
                             <tr>
-                                <td style="padding-left:4em">
+                                <td style="padding-left:5em">
                                     @php 
                                         if($result['po_quantity']){
                                             $po_percentage = ($result['po_quantity'] / $result['budget_quantity']) * 100;
