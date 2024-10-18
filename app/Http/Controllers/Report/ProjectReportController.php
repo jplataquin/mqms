45410094da47b80e8acc822556e17027272a846a
jplataquin <jp.lataquin@gmail.com>
2024-10-18 17:50:04 +0800

    fix

--- app/Http/Controllers/Report/ProjectReportController.php
+++ app/Http/Controllers/Report/ProjectReportController.php
@@ -36,6 +36,7 @@ class ProjectReportController extends Controller {
 
     public function generate(Request $request){
 
+        echo $request->url();exit;
         $project_id             = $request->input('project_id');
         $section_id             = $request->input('section_id');
         $contract_item_id       = (int) $request->input('contract_item_id');
