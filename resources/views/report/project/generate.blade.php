1d3f87935b45d00a0b2e8fb84c15a66be9d5bc88
jplataquin <jp.lataquin@gmail.com>
2024-10-26 21:10:35 +0800

    fix

--- resources/views/report/project/generate.blade.php
+++ resources/views/report/project/generate.blade.php
@@ -128,7 +128,7 @@
                     @foreach($component as $component_item_id => $component_item)
                         <tr>
                             <th style="padding-left:2em" class="component_item" data-amount={{ $component_item_arr[$component_item_id]->quantity * $component_item_arr[$component_item_id]->budget_price }}>{{$component_item_arr[$component_item_id]->name}}</th>
-                            <th>P {{ number_format($component_item_arr[$component_item_id]->quantity * $component_item_arr[$component_item_id]->budget_price,2) }}</th>
+                            <th>(M) P {{ number_format($component_item_arr[$component_item_id]->quantity * $component_item_arr[$component_item_id]->budget_price,2) }}</th>
                         </tr>
 
                         @foreach($component_item as $material_quantity_id => $result)
