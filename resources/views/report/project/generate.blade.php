83b78f7ba5a8c742de1d9bc5f95426a25427391d
jplataquin <jp.lataquin@gmail.com>
2024-11-05 23:00:16 +0800

    fix

--- resources/views/report/project/generate.blade.php
+++ resources/views/report/project/generate.blade.php
@@ -56,7 +56,7 @@
 
 
         <div class="mb-5 row">
-            <div class="col-lg-6 col-md-6 text-center">
+            <div class="col-lg-6 col-md-6 text-center border border-rounded border-primary">
                 <h3>Total Budget</h3>
                 <h5 id="material_budget_grand_total" data-value="0">-</h5>
             
