3ad3ca31404c50f020c12d1bda77f0ce28258fd7
jplataquin <jp.lataquin@gmail.com>
2024-11-05 23:02:41 +0800

    fix

--- resources/views/report/project/generate.blade.php
+++ resources/views/report/project/generate.blade.php
@@ -57,14 +57,16 @@
 
         <div class="mb-5 row">
             <div class="col-lg-6 col-md-6 text-center">
-                <div class="border rounded border-primary">
+                <div class="border rounded border-primary p-3">
                     <h3>Total Budget</h3>
                     <h5 id="material_budget_grand_total" data-value="0">-</h5>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-6 text-center">
-                <h3>Total Expense</h3>
-                <h5 id="material_expense_grand_total" class="check" data-check-target="#material_budget_grand_total" data-value="0">-</h5>
+            <div class="col-lg-6 col-md-6 text-center p-3">
+                <div class="border rounded border-primary">
+                    <h3>Total Expense</h3>
+                    <h5 id="material_expense_grand_total" class="check" data-check-target="#material_budget_grand_total" data-value="0">-</h5>
+                </div>
             </div>
         </div>
         <div class="mb-5 row">
