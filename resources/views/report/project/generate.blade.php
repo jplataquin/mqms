324a72829f9097fd13ca6a59c6e815587d4d6885
jplataquin <jp.lataquin@gmail.com>
2024-11-06 15:12:51 +0800

    fix

--- resources/views/report/project/generate.blade.php
+++ resources/views/report/project/generate.blade.php
@@ -102,8 +102,8 @@
 
                 <table class="horizontal-bar-stacked">
                     <tr>
-                        <td style="width: 0%" class="horizontal-bar-stacked-expense">40%</td>
-                        <td style="width: 0%" class="horizontal-bar-stacked-overhead">10%</td>
+                        <td style="width: 0%" class="horizontal-bar-stacked-expense"></td>
+                        <td style="width: 0%" class="horizontal-bar-stacked-overhead"></td>
                         <td style="width: 100%" class="horizontal-bar-stacked-default">50%</td>
                     </tr>
                 </table>
