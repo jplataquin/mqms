6909a5707450f1528e0ee91dd3c5b8ba292870d8
jplataquin <jp.lataquin@gmail.com>
2024-11-14 08:08:37 +0800

    fix

--- resources/views/material_quantity/report.blade.php
+++ resources/views/material_quantity/report.blade.php
@@ -75,9 +75,10 @@
             <div class="col-lg-6 mb-3">
                 <h4>Pending ({{ number_format($mqr_pending->total_quantity,2) }})</h4>
                 <div class="list-group">
-                    here{{ implode(',',$mqr_pending->mqr_ids) }}test
+
                     @foreach($mqr_pending->mqr_ids as $mqr_pending_id)
-                    
+
+                        @php if(!$mqr_pending_id) continue; @endphp
                         <a class="list-group-item list-group-item-action" href="/material_quantity_request/{{$mqr_pending_id}}">{{str_pad($mqr_pending_id,6,0,STR_PAD_LEFT)}}</a>
                     
                     @endforeach
