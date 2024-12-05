6205dcfb214ee15d375c59b22e158ef775ad01c9
jplataquin <jp.lataquin@gmail.com>
2024-12-05 10:03:49 +0800

    fix

--- resources/ui_components/CreateComponentItemForm.js
+++ resources/ui_components/CreateComponentItemForm.js
@@ -50,31 +50,31 @@ class CreateComponentItemForm extends ComponentV2{
                 t.div({class:'col-lg-2'},()=>{
                     t.div({class:'form-group'},()=>{
                         t.label('Variable');
-                        t.input({class:'form-control',type:'text'});
+                        this.el.variable = t.input({class:'form-control',type:'text'});
                     });
                 }); //div col
                 t.div({class:'col-lg-2'},()=>{
                     t.div({class:'form-group'},()=>{
                         t.label('Quantity');
-                        t.input({class:'form-control',type:'text'});
+                        this.el.quantity = t.input({class:'form-control',type:'text'});
                     });
                 }); //div col
                 t.div({class:'col-lg-2'},()=>{
                     t.div({class:'form-group'},()=>{
                         t.label('Equivalent');
-                        t.input({class:'form-control',type:'text'});
+                        this.el.equivalent = t.input({class:'form-control',type:'text'});
                     });
                 }); //div col
                 t.div({class:'col-lg-2'},()=>{
                     t.div({class:'form-group'},()=>{
                         t.label('Unit');
-                        t.select({class:'form-select'});
+                        this.el.unit_id = t.select({class:'form-select'});
                     });
                 }); //div col
                 t.div({class:'col-lg-2'},()=>{
                     t.div({class:'form-group'},()=>{
                         t.label('Unit Price');
-                        t.input({class:'form-control',type:'text'});
+                        this.el.unit_price = t.input({class:'form-control',type:'text'});
                     });
                 }); //div col
             });//div row
@@ -86,79 +86,63 @@ class CreateComponentItemForm extends ComponentV2{
 
     controller(){
 
-        this.el.quantity.onkeypress = (e) =>{
-            return window.util.inputNumber(this.el.quantity,e,2,false);
-        }
-
-        this.el.use_count.onkeypress = (e) =>{
-            return window.util.inputNumber(this.el.use_count,e,2,false);
-        }
-
-        this.el.submit_btn.onclick = ()=>{
-            this.submit();
-          }
-  
-          this.el.cancel_btn.onclick = ()=>{
-              window.util.drawerModal.close();
-          }
-  
-          this.setupUnits();
+       
   
     }
 
     setupUnits(){
 
-        const t = new Template();
+        // const t = new Template();
 
-        this.el.unit.append(
-            t.option({value:''},' - ')
-        );
+        // this.el.unit.append(
+        //     t.option({value:''},' - ')
+        // );
 
     
 
-        for(let key in this._model.unit_options){
+        // for(let key in this._model.unit_options){
 
-            let item = this._model.unit_options[key];
+        //     let item = this._model.unit_options[key];
 
-            //Skip if deleted
-            if(item.deleted) continue;
+        //     //Skip if deleted
+        //     if(item.deleted) continue;
 
-            let unit_option    = t.option({value:item.id},item.text);
+        //     let unit_option    = t.option({value:item.id},item.text);
 
 
-            this.el.unit.append(
-                unit_option
-            );
+        //     this.el.unit.append(
+        //         unit_option
+        //     );
 
-        };
+        // };
     }
 
 
     submit(){
 
-        window.util.blockUI();
+        // window.util.blockUI();
 
-        window.util.$post('/api/component/create',{
-            section_id          : this._model.section_id,
-            contract_item_id    : this._model.contract_item_id,
-            name                : this.el.name.value,
-            quantity            : this.el.quantity.value,
-            use_count           : this.el.use_count.value,
-            unit_id             : this.el.unit.value,
-            sum_flag            : (this.el._sum_flag.checked == true) ? 1 : 0
-        }).then(reply=>{
+        // window.util.$post('/api/component/create',{
+        //     section_id          : this._model.section_id,
+        //     contract_item_id    : this._model.contract_item_id,
+        //     name                : this.el.name.value,
+        //     quantity            : this.el.quantity.value,
+        //     use_count           : this.el.use_count.value,
+        //     unit_id             : this.el.unit.value,
+        //     sum_flag            : (this.el._sum_flag.checked == true) ? 1 : 0
+        // }).then(reply=>{
 
-            window.util.unblockUI();
+        //     window.util.unblockUI();
 
-            if(reply.status <= 0){
-                window.util.showMsg(reply);
-                return false;
-            }
+        //     if(reply.status <= 0){
+        //         window.util.showMsg(reply);
+        //         return false;
+        //     }
 
-            //Component(reply.data.id)).to(component_list)
-            this._model.callback(reply.data.id);
-            window.util.drawerModal.close();
-        });
+        //     //Component(reply.data.id)).to(component_list)
+        //     this._model.callback(reply.data.id);
+        //     window.util.drawerModal.close();
+        // });
 
     }
 }
