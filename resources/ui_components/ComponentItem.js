ff0188abc082bbcd3c682395c51c43b9a070f6fa
jplataquin <jp.lataquin@gmail.com>
2024-12-06 16:19:55 +0800

    fix

--- resources/ui_components/ComponentItem.js
+++ resources/ui_components/ComponentItem.js
@@ -342,7 +342,7 @@ class ComponentItem extends ComponentV2{
 
 
 
-        return t.div({class:'form-container mb-5 shadow bg-white rounded'},(el)=>{
+        return t.div({class:'form-container mb-5 shadow-lg bg-white rounded'},(el)=>{
             
             t.div({class:'form-header'},'');
 
