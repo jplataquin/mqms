1e16eb1693f1db8875e73f18eef3d702443d5349
jplataquin <jp.lataquin@gmail.com>
2024-12-06 15:56:59 +0800

    fix

--- resources/ui_components/MaterialQuantityList.js
+++ resources/ui_components/MaterialQuantityList.js
@@ -159,7 +159,13 @@ class MaterialQuantityList extends ComponentV2{
                         
                         this.el.material_quantity_item_container = t.tbody(()=>{});
 
-                        t.tfoot()
+                        t.tfoot(()=>{
+                            t.tr(()=>{
+                                t.td({class:'text-end',colspan:5},()=>{
+                                    this.el.addBtn = t.button({class:'btn btn-warning w-100'},'Add');
+                                });
+                            });
+                        });//foot
                         
                     });
                 });
