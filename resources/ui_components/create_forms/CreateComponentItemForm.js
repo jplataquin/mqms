21cee25afa792343a6ea6182688fbda164731613
jplataquin <jp.lataquin@gmail.com>
2024-12-08 23:31:53 +0800

    fix

--- resources/ui_components/create_forms/CreateComponentItemForm.js
+++ resources/ui_components/create_forms/CreateComponentItemForm.js
@@ -189,163 +189,164 @@ class CreateComponentItemForm extends ComponentV2{
         const t= new Template();
 
         return t.div(()=>{
-            
-            t.table({class:'table'},()=>{
-                    
-
-                t.tbody(()=>{
-                    t.tr(()=>{
-                        t.th({colspan:4},'Name');
-                        t.th({colspan:1},'Sum Flag');
-                    });
-    
-                    t.tr(()=>{
-                        t.td({colspan:4},()=>{
-                            this.el.name = t.input({class:'form-control name',type:'text'}); 
-                        });
-    
-                        t.td({colspan:1},()=>{
-                            t.div({class:'form-switch text-center'},()=>{                  
-                                this.el.sum_flag = t.input({class:'form-check-input sum_flag',value:1,type:'checkbox',checked:true});
-                            });
-                        });
+            t.div({class:'table-responsive'},()=>{
+                t.table({class:'table'},()=>{
                         
-                    });
-                    
-                    t.tr(()=>{
-                        t.th({colspan:5,class:'text-center bg-divider'},'POW/DUPA')
-                    });
 
-                    t.tr(()=>{
-                        t.th({colspan:2},'Quantity');
-                        t.th({colspan:1},'Unit');
-                        t.th({colspan:2},'Unit Price');
-                    });
-            
-                    t.tr(()=>{
-                        
-                        t.td({colspan:2},()=>{
-                            this.el.ref_1_quantity = t.input({class:'form-control'});
+                    t.tbody(()=>{
+                        t.tr(()=>{
+                            t.th({colspan:4},'Name');
+                            t.th({colspan:1},'Sum Flag');
                         });
-            
-                        t.td({colspan:1},()=>{
-            
-                            this.el.ref_1_unit_id = t.select({class:'form-control'},()=>{
-                                
-                                t.option({value:''},' - ');
-            
-                                for(let i in this._model.unit_options){
-            
-                                    if(this._model.unit_options[i].deleted){
-                                        t.option({value:i,disabled:true},this._model.unit_options[i].text+' [Deleted]');
-                                    }else{
-                                        t.option({value:i}, this._model.unit_options[i].text );
-                                    }
-                                   
-                                }
-                            });
-            
-                        });
-            
-                        t.td({colspan:2},()=>{
-                            this.el.ref_1_unit_price = t.input({class:'form-control'});
-                        });
-                    });
-
-                    t.tr(()=>{
-                        t.th({colspan:5,class:'text-center bg-divider'},'Material Budget')
-                    });
-    
-                    t.tr(()=>{
-                        t.th('Function Type');
-                        t.th('Variable');
-                        t.th('Quantity');
-                        t.th('Unit');
-                        t.th('Equivalent');
-                    })
-                    
-                    t.tr(()=>{
-                        
-                        t.td({class:''},(el)=>{
-                            
-                            this.el.function_type = t.select({class:'form-control function_type'},()=>{
-                                t.option({value:3,selected:true},'As Direct');
-                                t.option({value:4},'As Equivalent');
-                                t.option({value:1},'As Factor');
-                                t.option({value:2},'As Divisor');
-                                
-                            });
-    
-                        });
-    
-                         
-                        t.td({class:''},(el)=>{
-                            
-                            this.el.variable = t.input({class:'form-control variable', type:'text'});
-    
-                        });
-
-                        t.td({class:''},(el)=>{
-                            
-                            this.el.quantity = t.input({class:'form-control quantity', type:'text'});
-    
-                        });
-    
-                       
-    
-                        t.td({class:''},(el)=>{
-    
-                            this.el.unit = t.select({class:'form-control unit'},()=>{
-                                
-                                for(let i in this._model.unit_options){
-    
-                                    if(this._model.unit_options[i].deleted){
-                                        t.option({value:i,disabled:true},this._model.unit_options[i].text+' [Deleted]');
-                                    }else{
-                                        t.option({value:i}, this._model.unit_options[i].text );
-                                    }
-                                }
+        
+                        t.tr(()=>{
+                            t.td({colspan:4},()=>{
+                                this.el.name = t.input({class:'form-control name',type:'text'}); 
                             });
         
-                        });
-    
-
-                        t.td({class:''},(el)=>{
+                            t.td({colspan:1},()=>{
+                                t.div({class:'form-switch text-center'},()=>{                  
+                                    this.el.sum_flag = t.input({class:'form-check-input sum_flag',value:1,type:'checkbox',checked:true});
+                                });
+                            });
                             
-                            this.el.equivalent = t.input({class:'form-control equivalent', type:'text'});
-    
                         });
-    
-                       
-                    });
+                        
+                        t.tr(()=>{
+                            t.th({colspan:5,class:'text-center bg-divider'},'POW/DUPA')
+                        });
+
+                        t.tr(()=>{
+                            t.th({colspan:2},'Quantity');
+                            t.th({colspan:1},'Unit');
+                            t.th({colspan:2},'Unit Price');
+                        });
+                
+                        t.tr(()=>{
+                            
+                            t.td({colspan:2},()=>{
+                                this.el.ref_1_quantity = t.input({class:'form-control'});
+                            });
+                
+                            t.td({colspan:1},()=>{
+                
+                                this.el.ref_1_unit_id = t.select({class:'form-control'},()=>{
+                                    
+                                    t.option({value:''},' - ');
+                
+                                    for(let i in this._model.unit_options){
+                
+                                        if(this._model.unit_options[i].deleted){
+                                            t.option({value:i,disabled:true},this._model.unit_options[i].text+' [Deleted]');
+                                        }else{
+                                            t.option({value:i}, this._model.unit_options[i].text );
+                                        }
+                                    
+                                    }
+                                });
+                
+                            });
+                
+                            t.td({colspan:2},()=>{
+                                this.el.ref_1_unit_price = t.input({class:'form-control'});
+                            });
+                        });
+
+                        t.tr(()=>{
+                            t.th({colspan:5,class:'text-center bg-divider'},'Material Budget')
+                        });
+        
+                        t.tr(()=>{
+                            t.th('Function Type');
+                            t.th('Variable');
+                            t.th('Quantity');
+                            t.th('Unit');
+                            t.th('Equivalent');
+                        })
+                        
+                        t.tr(()=>{
+                            
+                            t.td({class:''},(el)=>{
+                                
+                                this.el.function_type = t.select({class:'form-control function_type'},()=>{
+                                    t.option({value:3,selected:true},'As Direct');
+                                    t.option({value:4},'As Equivalent');
+                                    t.option({value:1},'As Factor');
+                                    t.option({value:2},'As Divisor');
+                                    
+                                });
+        
+                            });
+        
+                            
+                            t.td({class:''},(el)=>{
+                                
+                                this.el.variable = t.input({class:'form-control variable', type:'text'});
+        
+                            });
+
+                            t.td({class:''},(el)=>{
+                                
+                                this.el.quantity = t.input({class:'form-control quantity', type:'text'});
+        
+                            });
+        
+                        
+        
+                            t.td({class:''},(el)=>{
+        
+                                this.el.unit = t.select({class:'form-control unit'},()=>{
+                                    
+                                    for(let i in this._model.unit_options){
+        
+                                        if(this._model.unit_options[i].deleted){
+                                            t.option({value:i,disabled:true},this._model.unit_options[i].text+' [Deleted]');
+                                        }else{
+                                            t.option({value:i}, this._model.unit_options[i].text );
+                                        }
+                                    }
+                                });
+            
+                            });
+        
+
+                            t.td({class:''},(el)=>{
+                                
+                                this.el.equivalent = t.input({class:'form-control equivalent', type:'text'});
+        
+                            });
+        
+                        
+                        });
+                        
+                        t.tr(()=>{
+                            t.th({colspan:2},'Unit Price');
+                            t.th({colspan:5},'Total Amount');
+                        });
+        
+                        t.tr(()=>{
+
+                            
+                            t.td({colspan:2},(el)=>{
+                                
+                                this.el.budget_price = t.input({class:'form-control budget_price', type:'text'});
+                                
+                            });
+                            
+                            t.td({colspan:5},()=>{
+                                this.el.total_amount = t.input({class:'form-control',disabled:true});
+                            });
+                        });
+
+
                     
-                    t.tr(()=>{
-                        t.th({colspan:2},'Unit Price');
-                        t.th({colspan:5},'Total Amount');
-                    });
-    
-                    t.tr(()=>{
-
-                        
-                        t.td({colspan:2},(el)=>{
-                            
-                            this.el.budget_price = t.input({class:'form-control budget_price', type:'text'});
-                            
-                        });
-                        
-                        t.td({colspan:5},()=>{
-                            this.el.total_amount = t.input({class:'form-control',disabled:true});
-                        });
-                    });
-
-
-                   
-                   
-    
-                });//tbody
-    
-    
-            });//table
+                    
+        
+                    });//tbody
+        
+        
+                });//table
+            });
     
             t.div({class:'row mb-3'},()=>{
                 t.div({class:'col-lg-12 text-end'},()=>{
