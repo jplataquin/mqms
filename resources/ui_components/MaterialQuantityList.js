575a96498ff307fbd5806345f1343ff294caa2c2
jplataquin <jp.lataquin@gmail.com>
2024-12-08 17:52:46 +0800

    fix

--- resources/ui_components/MaterialQuantityList.js
+++ resources/ui_components/MaterialQuantityList.js
@@ -21,39 +21,6 @@ class MaterialQuantityList extends ComponentV2{
 
     state(){
         return {
-            material_item:{
-                value:'',
-                target: this.el.material_item_select,
-                events:['change']
-            },
-            equivalent:{
-                value:0,
-                target:this.el.equivalent,
-                events:['keyup'],
-                getValue:(val)=>{
-                    return window.util.pureNumber(val);
-                },
-                onUpdate:(data)=>{
-
-                    this.setState('total',
-                        window.util.pureNumber(data.value) * this.getState('quantity')
-                    );
-                }
-            },
-            quantity:{
-                value:0,
-                target:this.el.quantity,
-                events:['keyup','change'],
-                getValue:(val)=>{
-                    return window.util.pureNumber(val);
-                },
-                onUpdate:(data)=>{
-
-                    this.setState('total',
-                        window.util.pureNumber(data.value) * this.getState('equivalent')
-                    );
-                }
-            },
             grand_total:{
                 value:0,
                 target:this.el.grand_total,
@@ -111,53 +78,8 @@ class MaterialQuantityList extends ComponentV2{
 
     controller(){
 
-        window.util.numbersOnlyInput([
-            this.el.quantity,
-            this.el.equivalent
-        ],{
-            precision:2
-        });
-
-        
-        this.el.addBtn.onclick = (e)=>{
-            e.preventDefault();
-            this.addMaterialQuantity();
-        }
 
         this.getMaterialQuantityList();
-    }
-
-    addMaterialQuantity(){
-
-        let data = {
-            component_item_id   : this._model.component_item_id,
-            material_item_id    : this.getState('material_item'),
-            quantity            : this.getState('quantity'),
-            equivalent          : this.getState('equivalent')
-        };
-
-        
-        window.util.blockUI();
-
-        window.util.$post('/api/material_quantity/create',data).then(reply=>{
-            
-            window.util.unblockUI();
-                
-            if(reply.status <= 0){
-                window.util.showMsg(reply);
-                return false;
-            }
-
-            
-            // this.appendMaterial({
-            //     id: reply.data.id,
-            //     material_item_id: data.material_item_id,
-            //     quantity: data.quantity,
-            //     equivalent: data.equivalent
-            // });
-
-            
-        });
     }
 
     getMaterialQuantityList(){
