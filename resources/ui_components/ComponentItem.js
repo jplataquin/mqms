8b2259610fb5f4635cb3f875eedfec21ff79da02
jplataquin <jp.lataquin@gmail.com>
2024-12-10 02:57:34 +0800

    fix

--- resources/ui_components/ComponentItem.js
+++ resources/ui_components/ComponentItem.js
@@ -286,7 +286,7 @@ class ComponentItem extends ComponentV2{
 
                                         t.div({class:'col-lg-1 col-sm-1'},()=>{
                                             t.div({class:'form-group'},()=>{
-                                               
+                                                t.br();
                                                 t.h4(()=>{
                                                     t.a({class:'me-5',href:'#'},()=>{
                                                         this.el.report_btn = t.i({class:'bi bi-list-task'});
