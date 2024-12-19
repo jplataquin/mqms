61114f4c5d03a7170ce033d22ae956f51b67f1f1
jplataquin <jp.lataquin@gmail.com>
2024-12-19 17:36:46 +0800

    fix

--- app/Http/Controllers/Report/ProjectReportController.php
+++ app/Http/Controllers/Report/ProjectReportController.php
@@ -315,12 +315,12 @@ class ProjectReportController extends Controller {
             'as_of_display'         => $as_of_display,
             'url'                   => $url,
 
-            'project_id'            => $project_id,
-            'section_id'            => $section_id,
-            'contract_item_id'      => $contract_item_id,
-            'component_id'          => $component_id,
-            'as_of'                 => $as_of,
-            'material_items_req'    => $request->input('material_items')
+            'project_id'                => $project_id,
+            'section_id'                => $section_id,
+            'contract_item_id'          => $contract_item_id,
+            'component_id'              => $component_id,
+            'as_of'                     => $as_of,
+            'material_items_request'    => $request->input('material_items')
         ];
     }
 
