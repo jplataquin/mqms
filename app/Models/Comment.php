74feb32bf7b523aaa743459c2096b0f00ac5bde3
jplataquin <jp.lataquin@gmail.com>
2024-12-24 16:35:00 +0800

    fix

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -31,4 +31,9 @@ class Comment extends Model
 
         return $user; 
     }
+
+    public function getCreatedAtAttribute($date)
+    {
+        return Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y-m-d H:i:s');
+    }
 }
