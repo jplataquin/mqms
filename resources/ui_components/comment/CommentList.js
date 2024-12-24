7e042c98c355db33b673e452b29c0ad4ce2db64b
jplataquin <jp.lataquin@gmail.com>
2024-12-24 16:51:58 +0800

    fix

--- resources/ui_components/comment/CommentList.js
+++ resources/ui_components/comment/CommentList.js
@@ -54,11 +54,11 @@ class CommentList extends ComponentV2{
         data.map(item=>{
 
             this.el.container.append(
-                t.div({class:'mb-3 border border-primary rounded p-1'},()=>{
+                t.div({class:'mb-3 border border-primary rounded ps-1 pe-1 pt-1'},()=>{
     
                     t.pre(item.content);
                     t.div({class:'text-end'},()=>{
-                        t.p({class:'font-weight-light font-italic blockquote-footer'},'By '+item.user.name+' '+item.created_at);
+                        t.p({class:'font-weight-light font-italic blockquote-footer'},item.user.name+' '+item.created_at);
                     });
                 })
             );
