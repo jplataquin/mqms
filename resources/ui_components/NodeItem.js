7d21d6d8bbf05fedbce1adef1a5108348f723f84
jplataquin <jp.lataquin@gmail.com>
2025-03-01 02:21:41 +0800

    fix

--- resources/ui_components/NodeItem.js
+++ resources/ui_components/NodeItem.js
@@ -121,8 +121,9 @@ class Item extends ComponentV2 {
 
             let parent_bound = this._model.parentContainer.getBoundingClientRect();
 
-            
+            console.log(label_bound,parent_bound);
             console.log(label_bound.y,parent_bound.y);
+            console.log(this.el.label.offsetTop);
             
             highlight.style.top     = (label_bound.y-2)+'px';
             highlight.style.left    = '0px';
