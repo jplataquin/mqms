5a6f6f541787c36a9e5e22076258e4f826ef8965
jplataquin <jp.lataquin@gmail.com>
2025-03-09 18:30:34 +0800

    fix

--- app/Http/Controllers/ProjectStudio/OverviewController.php
+++ app/Http/Controllers/ProjectStudio/OverviewController.php
@@ -14,12 +14,23 @@ use App\Models\Unit;
 class OverviewController extends Controller
 {
 
-    public function display($project_id){
+    public function display($project_id,$section_id){
 
         $project = Project::findOrFail($project_id);
+        $section = Section::where('project_id',$project_id)->where('id',$section_id)->where('deleted_at',null)->first();
 
+        if(!$section){
+           return abort(404);  
+        }
+
+        $data = [];
+
+        $contract_items = $section->ContractItems;
+
+        
         return view('/project_studio/overview',[
-            'project' => $project
+            'project' => $project,
+            'section' => $section
         ]);
     }
 }
