7955d4b3ea84a6847b5bfa7c2f3091392b5022db
jplataquin <jp.lataquin@gmail.com>
2025-03-13 00:42:09 +0800

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'access_codes' =>         \App\Http\Middleware\CurrentUser::class
+        'access_codes' => \App\Http\Middleware\AccessCodes::class
     ];
 }
--- app/Http/Middleware/AccessCodes.php
+++ app/Http/Middleware/AccessCodes.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class CurrentUser
+class AccessCodes
 {
     /**
      * Handle an incoming request.
