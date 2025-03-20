03d92394b8da24f2a8a7603a0367a1a28e31e97c
jplataquin <jp.lataquin@gmail.com>
2025-03-21 01:21:54 +0800

    fix

--- resources/views/report/purchase/generate.blade.php
+++ resources/views/report/purchase/generate.blade.php
@@ -25,33 +25,35 @@
 
    
         @foreach($data as $supplier_id => $d)
-        <h2>{{$d['supplier']->name}}</h2>
-        
-        <table class="table w-100">
-            <tr>
-                <th>Material Item</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Total</th>
-            </tr>
-            @foreach($d['items'] as $po_item)
-            <tr>
-                <td>
-                    {{$po_item->MaterialItem->formatted_name}}
-                </td>
-                <td class="text-center">
-                    {{ number_fromat($po_item->total_quantity,2) }}
-                </td>
-                <td class="text-end">
-                    P {{$po_item->price}}
-                </td>
-                <td class="text-end">
-                    P {{ number_format( ($po_item->total_quantity * $po_item->price), 2) }}
-                </td>
-            </tr>
-            @endforeach
-        </table>
+        <div class="mb-5">
 
+            <h2>{{$d['supplier']->name}}</h2>
+        
+            <table class="table w-100">
+                <tr>
+                    <th>Material Item</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Total</th>
+                </tr>
+                @foreach($d['items'] as $po_item)
+                <tr>
+                    <td>
+                        {{$po_item->MaterialItem->formatted_name}}
+                    </td>
+                    <td class="text-center">
+                        {{ number_fromat($po_item->total_quantity,2) }}
+                    </td>
+                    <td class="text-end">
+                        P {{$po_item->price}}
+                    </td>
+                    <td class="text-end">
+                        P {{ number_format( ($po_item->total_quantity * $po_item->price), 2) }}
+                    </td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
         @endforeach
     </div>
 
