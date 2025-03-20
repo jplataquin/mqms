3fc6706e83fd809df3df794c76f775fa8702ffdd
jplataquin <jp.lataquin@gmail.com>
2025-03-21 00:06:59 +0800

    fix

--- resources/views/report/purchase/parameters.blade.php
+++ resources/views/report/purchase/parameters.blade.php
@@ -370,7 +370,7 @@
                 query>append('to',to_val);
             }
 
-            window.open('/report/price/generate?'+query,'_blank').focus();
+            window.open('/report/purchase/generate?'+query,'_blank').focus();
         }
 
 
