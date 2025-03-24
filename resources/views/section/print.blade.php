00e102f3793f35264373a4f271f55b801bdc588a
JP Lataquin <jp.lataquin@gmail.com>
2025-03-24 23:54:54 +0800

    fix

--- resources/views/section/print.blade.php
+++ resources/views/section/print.blade.php
@@ -300,10 +300,12 @@
 
                         if($section->gross_total_amount > 0){
                             $contract_item_budget_percentage = ($contract_item_budget_total_amount / $section->gross_total_amount) / 100;
-                            echo $contract_item_budget_percentage;
+                          
                         }
 
                         $contract_item_budget_percentage = number_format($contract_item_budget_percentage,2);
+
+                        echo $contract_item_budget_percentage;
                     @endphp
                  
                     {{$contract_item_budget_percentage}}%
