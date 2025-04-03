58d230fe15f7d7d3d56b21cac3e2fe438a273fa1
JP Lataquin <jp.lataquin@gmail.com>
2025-04-03 23:02:00 +0800

    fix

--- resources/views/print/budget.blade.php
+++ resources/views/print/budget.blade.php
@@ -208,7 +208,7 @@
         @if($hide['total_contract_item'] > 0 || $hide['total_component'] > 0)
         <tr>
             <th data-controller="pageBreaker" class="text-center rejected-text" colspan="8">
-                Hidden Contract Items: {{ number_format($hide['total_contract_item']) }}, 
+                Hidden Contract Items: {{ number_format($hide['total_contract_item']) }}
             </th>
             <th class="text-center rejected-text" colspan="8">
                 Hidden Components: {{ number_format($hide['total_component']) }}
