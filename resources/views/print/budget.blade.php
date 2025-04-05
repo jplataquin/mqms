a11720ce6190407a6432752e92ec36932403505a
JP Lataquin <jp.lataquin@gmail.com>
2025-04-06 00:08:46 +0800

    fix

--- resources/views/print/budget.blade.php
+++ resources/views/print/budget.blade.php
@@ -483,7 +483,7 @@
                         @elseif($component_item->approximation == 'FLOR')
                         ↓ 
                         @endif
-                        {{$component_item->approximation}} X
+                        
                         {{ number_format( $component_item->quantity,2) }}
                         
                     </td>
