c37e29538990eb83be13c18a34db923948f683b1
JP Lataquin <jp.lataquin@gmail.com>
2025-04-07 08:43:22 +0800

    fix

--- resources/views/review/budget/sheet.blade.php
+++ resources/views/review/budget/sheet.blade.php
@@ -938,7 +938,7 @@
 
 
                     console.log(sheet_pos);
-                    console.log(body.scrollTop(),body.scrollLeft());
+                    console.log(document.scrollTop(),document.scrollLeft());
                     // let pos = item.getBoundingClientRect();
                     let posX = e.clientX + sheet_pos.left;
                     let posY = e.clientY + sheet_pos.top;
