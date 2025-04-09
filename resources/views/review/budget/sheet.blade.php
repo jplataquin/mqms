b2857aa72f92365e0e2ed890353ad42e4d7d5dd8
jplataquin <jp.lataquin@gmail.com>
2025-04-09 14:25:05 +0800

    fix

--- resources/views/review/budget/sheet.blade.php
+++ resources/views/review/budget/sheet.blade.php
@@ -6,7 +6,9 @@
     <title>{{$project->name}} - {{$section->name}} - {{$datetime_generated}}</title>
     <style>
 
-        
+        *:hover{
+            border: solid 3px green !important;
+        }
 
         .d-none{
             display:none;
@@ -983,26 +985,26 @@
 
     <div id="actionContainer">
         
-        <button id="actionBtn" class="me-10 no-select">
+        <div id="actionBtn" class="me-10 no-select">
             
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-fullscreen-exit no-select" viewBox="0 0 16 16">
                 <path d="M5.5 0a.5.5 0 0 1 .5.5v4A1.5 1.5 0 0 1 4.5 6h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5m5 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 10 4.5v-4a.5.5 0 0 1 .5-.5M0 10.5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 6 11.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5m10 1a1.5 1.5 0 0 1 1.5-1.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0z"/>
             </svg>
         
-        </button>
+        </div>
 
-        <button id="collapseBtn" class="me-10 no-select">
+        <div id="collapseBtn" class="me-10 no-select">
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrows-collapse no-select" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8m7-8a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 4.293V.5A.5.5 0 0 1 8 0m-.5 11.707-1.146 1.147a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 11.707V15.5a.5.5 0 0 1-1 0z"/>
             </svg>
 
-        </button>
+        </div>
 
-        <button id="searchBtn" class="no-select">
+        <div id="searchBtn" class="no-select">
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-search no-select" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
             </svg>
-        </button>
+        </div>
     </div>
 
     <script type="module">
