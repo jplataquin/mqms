da96ad40bbc575a23f4798ad2b4558ffed31d1e3
jplataquin <jp.lataquin@gmail.com>
2025-04-21 14:33:25 +0800

    fix

--- resources/views/review/budget/display.blade.php
+++ resources/views/review/budget/display.blade.php
@@ -1,85 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
-<style>
 
-    .fullscreen{
-        position:absolute;
-        top:0;
-        left:0;
-        z-index:1000;
-    }
-
-    .no-scroll{
-        height:100%;
-        overflow:hidden;
-    }
-        
-</style>
 <div id="content">
-<div class="container">
-    <div class="breadcrumbs" hx-boost="true" hx-select="#content" hx-target="#main">
-        <ul>
-            <li>
-                <a href="/review/material_canvass">
-                    <span>
-                       Review
-                    </span>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="active">
-                    <span>
-                         Budget
-                    </span>
-                    <i class="ms-2 bi bi-display"></i>
-                </a>
-            </li>
-        </ul>
-    </div>
-    <hr>
+    <style>
 
-    <div style="height: 100vh">
-         <div class="text-end mb-5">
-            <button class="btn btn-secondary" id="fullScreenBtn">Full Screen</button>
+        .fullscreen{
+            position:absolute;
+            top:0;
+            left:0;
+            z-index:1000;
+        }
+
+        .no-scroll{
+            height:100%;
+            overflow:hidden;
+        }
+        
+    </style>
+
+    <div class="container">
+        <div class="breadcrumbs" hx-boost="true" hx-select="#content" hx-target="#main">
+            <ul>
+                <li>
+                    <a href="/review/material_canvass">
+                        <span>
+                        Review
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="active">
+                        <span>
+                            Budget
+                        </span>
+                        <i class="ms-2 bi bi-display"></i>
+                    </a>
+                </li>
+            </ul>
         </div>
-        <iframe id="sheet_iframe" class="w-100 h-100 d-inline-block" src="/review/budget/sheet/{{$section_id}}"></iframe>
+        <hr>
+
+        <div style="height: 100vh">
+            <div class="text-end mb-5">
+                <button class="btn btn-secondary" id="fullScreenBtn">Full Screen</button>
+            </div>
+            <iframe id="sheet_iframe" class="w-100 h-100 d-inline-block" src="/review/budget/sheet/{{$section_id}}"></iframe>
+        </div>
+    
     </div>
-  
-</div>
 
-<script type="module">
-    import {$q} from '/adarna.js';
-    
-    const sheet_iframe      = $q('#sheet_iframe').first();
-    const fullScreenBtn     = $q('#fullScreenBtn').first();
+    <script type="module">
+        import {$q} from '/adarna.js';
+        
+        const sheet_iframe      = $q('#sheet_iframe').first();
+        const fullScreenBtn     = $q('#fullScreenBtn').first();
 
-    window.getSheetPos = ()=>{
-        let data = sheet_iframe.getBoundingClientRect();
-        return data;
-    };
+        window.getSheetPos = ()=>{
+            let data = sheet_iframe.getBoundingClientRect();
+            return data;
+        };
 
-    window.reloadIframe = ()=>{
-        overview_iframe.contentWindow.location.reload();
-    }
+        window.reloadIframe = ()=>{
+            overview_iframe.contentWindow.location.reload();
+        }
 
-    window.exitFullscreen = ()=>{
-        sheet_iframe.classList.remove('fullscreen');
-        document.body.classList.remove('no-scroll');
-    }
+        window.exitFullscreen = ()=>{
+            sheet_iframe.classList.remove('fullscreen');
+            document.body.classList.remove('no-scroll');
+        }
 
-    fullScreenBtn.onclick = ()=>{
-        sheet_iframe.classList.add('fullscreen');
-        document.body.classList.add('no-scroll');
-    }
-    
+        fullScreenBtn.onclick = ()=>{
+            sheet_iframe.classList.add('fullscreen');
+            document.body.classList.add('no-scroll');
+        }
+        
 
-    window.util.quickNav = {
-        title:'Review Budget',
-        url: '/review/budget'
-    };
-    
-    
-</script>
+        window.util.quickNav = {
+            title:'Review Budget',
+            url: '/review/budget'
+        };
+        
+        
+    </script>
 </div>
 @endsection
