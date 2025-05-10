834eb6ee1671781412c49e9d801ad8554d6fe503
jplataquin <jp.lataquin@gmail.com>
2025-05-10 16:45:06 +0800

    fix

--- resources/ui_components/RequestMaterialItem.js
+++ resources/ui_components/RequestMaterialItem.js
@@ -407,7 +407,7 @@ class RequestMaterialItem extends Component{
                 return false;
             }
 
-            this.el.prevApprovedQuantity.value = window.util.numberFormat(reply.data.total_approved_quantity,2);
+            this.el.prevApprovedQuantity.value = window.util.numberFormat(reply.data.total_approved_quantity,2) + ' ' +reply.data.unit_text;
 
             let budget                         = window.util.pureNumber(this.el.materialBudgetQuantity.value);
             let approved                       = reply.data.total_approved_quantity;
