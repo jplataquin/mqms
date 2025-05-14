4199ad928a6b1aa5bc0605ba502c5ad4dcdf2013
jplataquin <jp.lataquin@gmail.com>
2025-05-15 04:01:19 +0800

    fix

--- resources/views/access_denied.blade.php
+++ resources/views/access_denied.blade.php
@@ -4,7 +4,7 @@
 <div id="content">
     <div class="container">
         <div class="text-center">
-            <h1>Access Denined</h1>
+            <h1>Access Denied</h1>
         </div>
     </div>
 </div>
