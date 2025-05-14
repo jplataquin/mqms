d6d5afe46ce94b92a32dbac25c3d4ddacc2d09bc
jplataquin <jp.lataquin@gmail.com>
2025-05-15 03:59:57 +0800

    fix

---
+++ resources/views/access_denied.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="content">
+    <div class="container">
+        <div class="text-center">
+            <h1>Access Denined</h1>
+        </div>
+    </div>
+</div>
+@endsection
