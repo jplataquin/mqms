f38eb1b444e889cb2c49d1f799ef19244deba13c
jplataquin <jp.lataquin@gmail.com>
2025-05-27 16:45:33 +0800

    fix

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -608,7 +608,7 @@ class ComponentController extends Controller
 
         //todo check role
 
-        $section_id         = (int) $request->input('section_id') ?? 0;
+        //$section_id         = (int) $request->input('section_id') ?? 0;
         $contract_item_id   = (int) $request->input('contract_item_id') ?? 0;
         $page               = (int) $request->input('page')     ?? 1;
         $limit              = (int) $request->input('limit')    ?? 0;
@@ -620,9 +620,9 @@ class ComponentController extends Controller
 
         $component = new Component();
 
-        if($section_id){
-            $component = $component->where('section_id',$section_id);
-        }
+        // if($section_id){
+        //     $component = $component->where('section_id',$section_id);
+        // }
 
         if($contract_item_id){
             $component = $component->where('contract_item_id',$contract_item_id);
