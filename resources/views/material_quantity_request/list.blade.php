9f65c96ca7748a7965772de444c009477709516b
jplataquin <jp.lataquin@gmail.com>
2025-06-05 16:41:47 +0800

    fix

--- resources/views/material_quantity_request/list.blade.php
+++ resources/views/material_quantity_request/list.blade.php
@@ -71,7 +71,7 @@
                 <div class="col-lg-4">
                     <div class="form-group">
                         <label>From</label>
-                        <input id="form" class="form-control"/>
+                        <input id="form" type="text" class="form-control"/>
                     </div>
                 </div>
                 <div class="col-lg-4">
@@ -196,6 +196,7 @@
         url:'/material_quantity_request'
     };
     
+    console.log(from,to);
     const datepicker_from = new Datepicker(from, {
        clearButton:true,
        format: { 
