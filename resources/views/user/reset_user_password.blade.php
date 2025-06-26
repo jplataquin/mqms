cc8370aa435fbca5541769719075ab9c92ade2b4
jplataquin <jp.lataquin@gmail.com>
2025-06-27 01:27:02 +0800

    fix

--- resources/views/user/reset_user_password.blade.php
+++ resources/views/user/reset_user_password.blade.php
@@ -1,83 +1,85 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div id="content">
+    <div class="container">
 
-    <div class="breadcrumbs" hx-boost="true" hx-select="#content" hx-target="#main">
-        <ul>
-            <li>
-                <a href="/users">
-                    <span>
-                       Users
-                    </span>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="active">
-                    <span>
-                        Reset Password
-                    </span>	
-                    <i class="ms-2 bi bi-display"></i>	
-                </a>
-            </li>
-        </ul>
-    </div>
-    <hr>
-
-    <div class="form-container">
-        <div class="form-header">
-            Reset User Password
+        <div class="breadcrumbs" hx-boost="true" hx-select="#content" hx-target="#main">
+            <ul>
+                <li>
+                    <a href="/users">
+                        <span>
+                        Users
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="active">
+                        <span>
+                            Reset Password
+                        </span>	
+                        <i class="ms-2 bi bi-display"></i>	
+                    </a>
+                </li>
+            </ul>
         </div>
-        <div class="form-body">
+        <hr>
 
-                <div class="row mb-3">
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label>Name</label>
-                            <input type="text" disabled="true" value="{{$user->name}}" class="form-control" id="name"/>
+        <div class="form-container">
+            <div class="form-header">
+                Reset User Password
+            </div>
+            <div class="form-body">
+
+                    <div class="row mb-3">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" disabled="true" value="{{$user->name}}" class="form-control" id="name"/>
+                            </div>
                         </div>
                     </div>
-                </div>
 
 
-                <div class="row mb-3">
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input type="text" value="{{$user->email}}" disabled="true" class="form-control" id="email"/>
+                    <div class="row mb-3">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="text" value="{{$user->email}}" disabled="true" class="form-control" id="email"/>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="row mb-3">
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input type="password" class="form-control" id="password"/>
+                    <div class="row mb-3">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="password" class="form-control" id="password"/>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="row mb-3">
-                    <div class="col-12">
-                        <div class="form-group">
-                            <label>Retype Password</label>
-                            <input type="password" class="form-control" id="repassword"/>
+                    <div class="row mb-3">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Retype Password</label>
+                                <input type="password" class="form-control" id="repassword"/>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="row">
-                    <div class="col-12 text-end">
-                        
-                        <button class="btn btn-primary" id="submitBtn">Submit</button>
+                    <div class="row">
+                        <div class="col-12 text-end">
+                            
+                            <button class="btn btn-primary" id="submitBtn">Submit</button>
+                        </div>
                     </div>
-                </div>
+            </div>
         </div>
-    </div>
-    <script type="module">
-        import {$q} from '/adarna.js';
+        <script type="module">
+            import {$q} from '/adarna.js';
 
-    </script>
+        </script>
+    </div>
 </div>
 @endsection
