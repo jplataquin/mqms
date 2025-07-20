aa37bf63648a03b2fcd0e8b99680c84a25a5981d
jplataquin <jp.lataquin@gmail.com>
2025-07-21 00:40:00 +0800

    fix

--- app/Http/Controllers/Report/FulfilmentReportController.php
+++ app/Http/Controllers/Report/FulfilmentReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Report;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\MaterialQuantityRequest;
 
 class FulfilmentReportController extends Controller
 {
@@ -18,7 +19,12 @@ class FulfilmentReportController extends Controller
 
         $from = $request->input('from');
         $to   = $request->input('to');
-        
+
+        $from = $from.' 00:00:00';
+        $to   = $to.' 23:59:59';
+
+        $material_quantity_request = MaterialQuantityRequest::where('status','APRV')->where('date_created','>=',$from)->where('date_created','<=',$to)->get();
+
         return view('report/fulfilment/generate');
     }
 }
