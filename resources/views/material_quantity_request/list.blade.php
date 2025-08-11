7d4fd17003034cb12261d2a5e7801f8da5bc256f
jplataquin <jp.lataquin@gmail.com>
2025-08-11 22:42:57 +0800

    fix

--- resources/views/material_quantity_request/list.blade.php
+++ resources/views/material_quantity_request/list.blade.php
@@ -160,7 +160,7 @@
                 <thead>
                        
                     <tr>
-                         <!--
+                   
                         <th style="min-width:50px">ID</th>
                       
                         <th style="min-width:50px">Status</th>
@@ -170,7 +170,7 @@
                         <th style="min-width:300px">Project</th>
                          
                         <th style="min-width:200px">Section</th>
-                          -->
+                      
                         <th style="min-width:300px">Contract Item</th>
                         
                         <th style="min-width:300px">Component</th>
