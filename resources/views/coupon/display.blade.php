1e16511eeb876eaad5740a82fbc813fd4044b7e0
jplataquin <jp.lataquin@gmail.com>
2025-09-20 16:04:44 +0800

    fix

--- resources/views/coupon/display.blade.php
+++ resources/views/coupon/display.blade.php
@@ -40,28 +40,21 @@
                 </div>                
             </div>
 
-              <div class="row">
-                <div class="col-lg-12">
-                    <div class="form-group">
-                        <label>Status</label>
-                        <input type="text" id="status" class="form-control" disabled="true" value="{{$coupon->status}}"/>
-                    </div>
-                </div>                
-            </div>
-
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="form-group">
-                        <label>Claimed By</label>
-                        <input type="text" id="amount" class="form-control" disabled="true" value="{{$coupon->amount}}"/>
-                    </div>
-                </div>                
-            </div>
+             
 
             <div class="row mt-5">
                 <div class="col-12 text-end">
-                <button class="btn btn-secondary" id="cancelBtn">Cancel</button>
-                <button class="btn btn-primary" id="createBtn">Create</button>
+                    <button class="btn btn-secondary" id="cancelBtn">Cancel</button>
+                   
+                    @if($coupon->status == 'PEND')    
+                        <button class="btn btn-danger" id="deleteBtn">Delete</button>    
+                        <button class="btn btn-primary" id="editBtn">Edit</button>
+                        <button class="btn btn-primary d-none" id="updateBtn">Update</button>
+                    @endif
+
+                    @if($coupon->status == 'APRV')    
+                        <button class="btn btn-primary" id="printBtn">Print</button>
+                    @endif
                 </div>
             </div>
         </div>
@@ -73,34 +66,41 @@
     import {$q} from '/adarna.js';
 
     const amount    = $q('#amount').first();
-    const createBtn = $q('#createBtn').first();
-    const cancelBtn = $q('#cancelBtn').first();
+
+    const deleteBtn  = $q('#deleteBtn').first();
+    const editBtn    = $q('#editBtn').first();
+    const updateBtn  = $q('#editBtn').first();
+    const printBtn   = $q('#printBtn').first();
+    
+    const cancelBtn  = $q('#cancelBtn').first();
     
     amount.onkeypress = (e)=>{
         return window.util.inputNumber(amount,e,2,false);
     }
 
-    createBtn.onclick = (e)=>{
-        window.util.blockUI();
-
-        window.util.$post('/api/coupon/create',{
-            amount : amount.value,           
-        }).then(reply=>{
-            
-            window.util.unblockUI();
-
-            if(reply.status <= 0 ){
-                window.util.showMsg(reply);
-                return false;
-            };
-    
-            window.util.navTo('/coupon/'+reply.data.id);
-        
-        });
-    }
-
     cancelBtn.onclick = (e) => {
         window.util.navTo('/coupons');
+    }
+
+    if(editBtn){
+        editBtn.onclick = (e) =>{
+            
+            editBtn.classList.add('d-none');
+            deleteBtn.classList.add('d-none');
+            updateBtn.classList.remove('d-none');
+
+            cancelBtn.onclick = (e) => {
+                window.util.navTo('/coupon/{{$coupon->id}}');
+            }
+
+            amount.disabled = false;
+        }
+    }
+
+    if(updateBtn){
+        updateBtn.onclick = (e) =>{
+            
+        }
     }
     
 </script>
