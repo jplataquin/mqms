4563399d82b27df3e9e5ed3e60790890d8831d9b
jplataquin <jp.lataquin@gmail.com>
2025-09-21 23:44:53 +0800

    fix

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Coupon;
+use App\Models\User;
 use Illuminate\Support\Str;
 
 class CouponController extends Controller
