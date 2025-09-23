2246663962be0623a1803b68a1376b480e397437
jplataquin <jp.lataquin@gmail.com>
2025-09-23 08:44:20 +0800

    fix

--- resources/views/coupon/generate.blade.php
+++ resources/views/coupon/generate.blade.php
@@ -45,7 +45,7 @@
 
                 let amount_text             = "P {{number_format($coupon->amount,2)}}";
                 
-                ctx.fillText(id_text,110, 10+90+10+75 );
+                ctx.fillText(amount_text,110, 10+90+10+75 );
             }
             
             qrImg.src       = "/qrcode?d={{ url('/coupon/claim/'.$coupon->id.'/'.$coupon->code ) }}";
