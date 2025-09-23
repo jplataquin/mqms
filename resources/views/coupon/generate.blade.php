a0a60bf9600c1eca3471feba2b2725d0b28ef3dd
jplataquin <jp.lataquin@gmail.com>
2025-09-23 09:07:00 +0800

    fix

--- resources/views/coupon/generate.blade.php
+++ resources/views/coupon/generate.blade.php
@@ -61,7 +61,7 @@
                 ctx.drawImage(headerImg, 110, 10, newWidth * .7,newHeight *.7);
                 
                 ctx.fillStyle           = 'black'; // Set fill color for the text
-                ctx.font                = "18px Arial";
+                ctx.font                = "16px Arial";
 
                 let amount_text             = "Fuel Coupon ( P {{number_format($coupon->amount,2)}} )";
                 
