156206554e49a02268d53d39a9a1f8cf6eade7b4
jplataquin <jp.lataquin@gmail.com>
2025-09-23 09:04:35 +0800

    fix

--- resources/views/coupon/generate.blade.php
+++ resources/views/coupon/generate.blade.php
@@ -61,11 +61,11 @@
                 ctx.drawImage(headerImg, 110, 10, newWidth * .7,newHeight *.7);
                 
                 ctx.fillStyle           = 'black'; // Set fill color for the text
-                ctx.font                = "14px Arial";
+                ctx.font                = "16px Arial";
 
                 let amount_text             = "Fuel Coupon ( P {{number_format($coupon->amount,2)}} )";
                 
-                ctx.fillText(amount_text, 10+90+10+75, 100 );
+                ctx.fillText(amount_text, 10+90+10+75, 90 );
             }
             
             qrImg.src       = "/qrcode?d={{ url('/coupon/claim/'.$coupon->id.'/'.$coupon->code ) }}";
