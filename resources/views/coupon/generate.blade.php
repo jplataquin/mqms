bfbeda5899e5f62a005c872febfe247c7d7239fd
jplataquin <jp.lataquin@gmail.com>
2025-09-23 08:59:36 +0800

    fix

--- resources/views/coupon/generate.blade.php
+++ resources/views/coupon/generate.blade.php
@@ -65,7 +65,7 @@
 
                 let amount_text             = "Fuel Coupon ( P {{number_format($coupon->amount,2)}} )";
                 
-                ctx.fillText(amount_text, 200, 100 );
+                ctx.fillText(amount_text, 200, 110 );
             }
             
             qrImg.src       = "/qrcode?d={{ url('/coupon/claim/'.$coupon->id.'/'.$coupon->code ) }}";
