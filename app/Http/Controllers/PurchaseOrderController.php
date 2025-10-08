d9bac2e38fccec728795328c733852a5a5f993ba
jplataquin <jp.lataquin@gmail.com>
2025-10-08 11:45:50 +0800

    fix

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -431,7 +431,7 @@ class PurchaseOrderController extends Controller
             if($remaining_quantity_arr[$po_item->material_item_id] < $po_item->quantity && $po->status == 'PEND'){
          
 
-                $po_item_arr[$po_item->material_item_id][] = 'Approved Material Request quantity is less than the PO item quantity '.$remaining_quantity_arr[$po_item->material_item_id].' < '.$po_item->quantity;
+                $po_item_arr[$po_item->material_item_id][] = 'Available Material Request quantity ('.$remaining_quantity_arr.') is less than the PO item quantity ('.$po_item->quantity.')';
             }
         }
 
