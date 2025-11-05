2012bbc5c61a80e5146b556b169b3585485de4e3
jplataquin <jp.lataquin@gmail.com>
2025-11-06 01:11:04 +0800

    fix

---
+++ resources/views/review/purchase_order/bulk/fail.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="content">
+    <div class="breadcrumbs" hx-boost="true" hx-select="#content" hx-target="#main">
+        <ul>
+            <li>
+                <a href="/review/purchase_orders">
+                    <span>
+                       Review
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="active">
+                    <span>
+                       Bulk PO
+                    </span>
+                    <i class="ms-2 bi bi-list-ul"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <hr>
+
+    <div class="text-center">
+        {{$message}}
+    </div>
+
+</div>
+@endsection
