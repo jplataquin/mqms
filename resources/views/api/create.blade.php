dc9d1add055143a77b79dc45d1d4ac3deba2efdd
JP Lataquin <jp.lataquin@gmail.com>
2026-05-27 17:32:55 +0800

    api

--- resources/views/api/create.blade.php
+++ resources/views/api/create.blade.php
@@ -136,7 +136,7 @@
         window.util.navTo('/api_credentials');
     }
 
-    $q('.copy-btn').map(btn => {
+    $q('.copy-btn').all().map(btn => {
         btn.onclick = () => {
             const targetId = btn.getAttribute('data-target');
             const input = document.getElementById(targetId);
